ccf31d8932854734cc0886aff1ebc90d594cebbb
Guillaume Wenzek <wzk@zml.ai>
2024-10-03 18:05:13 +0200

    minimal ClipTransformer

--- examples/sdxl/transformer.zig
+++ examples/sdxl/transformer.zig
@@ -11,146 +11,42 @@ const Tensor = zml.Tensor;
 const ShapeOf = zml.ShapeOf;
 const expectClose = zml.testing.expectClose;
 
-pub const LlamaOptions = struct {
-    num_heads: i64,
-    num_kv_heads: i64,
-    rms_norm_eps: f32,
-    rope_opts: zml.nn.RopeOpts,
-};
-
-/// ClipTextTransformer architecture, using huggingface transformers naming.
-/// Dimensions of activations: {.b, .s, .d}
-pub const LlamaLM = struct {
-    lm_head: zml.nn.Linear,
-    model: ClipTextTransformer,
-
-    pub fn init(self: *LlamaLM, options: LlamaOptions) void {
-        self.model.max_seq_len = options.max_seq_len;
-        self.model.num_heads = options.num_heads;
-        self.model.num_kv_heads = options.num_kv_heads;
-        self.model.rope_opts = options.rope_opts;
-        for (self.model.layers) |*layer| {
-            layer.self_attn.num_heads = options.num_heads;
-            layer.self_attn.num_kv_heads = options.num_kv_heads;
-            layer.self_attn.rope_opts = options.rope_opts;
-            layer.input_layernorm.eps = options.rms_norm_eps;
-            layer.post_attention_layernorm.eps = options.rms_norm_eps;
-        }
-    }
-
-    /// Predicts the token at `token_index` position.
-    /// Returns:
-    ///  - updated `tokens`,
-    ///  - `token_idx` + 1,
-    ///  - updated KV cache
-    ///  - a Rng state to allow for probabilistic generation
-    pub fn forward(
-        self: LlamaLM,
-        tokens_: Tensor,
-        token_index: Tensor,
-        rng: Tensor.Rng,
-    ) struct { Tensor, Tensor, Tensor.Rng } {
-        meta.assert(tokens_.dtype() == .i32 and tokens_.rank() >= 1 and token_index.dtype() == .i32 and token_index.rank() == 0, "Can't run ClipTextTransformer ! Expected >=1d tokens and 0d token_index, got: {} and {}", .{ tokens_, token_index });
-
-        var tokens = tokens_.withPartialTags(.{.s});
-        const out = zml.call(self.model, .forward, .{ tokens, null });
-        tokens, const new_rng = updateTokens(self.lm_head, tokens, token_index, out, rng, self.gen_opts);
-        return .{ tokens, increment(0, token_index), new_rng };
-    }
-
-    pub fn updateTokens(
-        lm_head: zml.nn.Linear,
-        tokens_: Tensor,
-        token_index: Tensor,
-        out_: Tensor,
-        rng: Tensor.Rng,
-        opts: zml.nn.SamplingStrategy,
-    ) struct { Tensor, Tensor.Rng } {
-        const tokens = tokens_.withPartialTags(.{.s});
-        const out = out_.withPartialTags(.{ .s, .d });
-
-        const next_token_pred = out.gatherValues(.s, token_index, .{});
-        var logits = zml.call(lm_head, .forward, .{next_token_pred});
-        if (logits.shape().hasTag(.voc) == null)
-            logits = logits.rename(.{ .d = .voc });
-
-        const next_token, const new_rng = zml.nn.sampleTokens(logits, opts, rng);
-        const next_token_index = token_index.addConstant(1);
-        const new_tokens = tokens.dynamicUpdateSlice(.{ .s = next_token_index }, next_token);
-
-        return .{ new_tokens.reuseBuffer(tokens_), new_rng };
-    }
-
-    pub fn increment(_: u8, token_index: Tensor) Tensor {
-        return token_index.addConstant(1).reuseBuffer(token_index);
-    }
+pub const ClipTextConfig = struct {
+    num_heads: u32,
+    max_position_embeddings: u32,
+    hidden_act: zml.nn.Activation = .gelu,
+    layer_norm_eps: f32 = 1e-5,
 };
 
 pub const ClipTextTransformer = struct {
-    embeddings: struct {
-        token_embedding: zml.nn.TokenEmbedding,
-        position_embedding: struct { weight: zml.Tensor },
-    },
+    embeddings: ClipTextEmbeddings,
     encoder: struct { layers: []TransformerLayer },
     final_layer_norm: zml.nn.LayerNorm,
 
-    max_seq_len: u32 = 0,
-    num_heads: i64 = 32,
-    num_kv_heads: i64 = 32,
-    rope_opts: zml.nn.RopeOpts = .{
-        .impl = .interleaved,
-        .freq_base = 10_000,
-    },
+    config: ClipTextConfig = undefined,
 
-    const Shape = struct {
-        s: u32,
-        layer: u16,
-        hd: u16,
-        nh: u16,
-        nkvh: u16,
-        dtype: zml.DataType,
-    };
-
-    pub fn shape(self: ClipTextTransformer) Shape {
-        const key_dim = self.layers[0].self_attn.k_proj.weight.dim(0);
-        const num_kv_heads = if (self.num_kv_heads > 0) self.num_kv_heads else self.num_heads;
-
-        return .{
-            .s = self.max_seq_len,
-            .layer = @intCast(self.layers.len),
-            .hd = @intCast(@divExact(key_dim, num_kv_heads)),
-            .nh = @intCast(self.num_heads),
-            .nkvh = @intCast(num_kv_heads),
-            .dtype = self.embed_tokens.weight.dtype(),
-        };
+    pub fn init(self: *ClipTextTransformer, config: ClipTextConfig) void {
+        self.config = config;
+        for (self.encoder.layers) |*layer| {
+            layer.self_attn.num_heads = config.num_heads;
+            layer.layer_norm1.eps = config.layer_norm_eps;
+            layer.layer_norm2.eps = config.layer_norm_eps;
+            layer.mlp.activation = config.hidden_act;
+        }
+        self.embeddings.max_position_embeddings = config.max_position_embeddings;
     }
 
     /// Forward one token, using KV cache for previous tokens.
     /// Returns result and updated KV cache.
-    pub fn forward(self: ClipTextTransformer, tokens: Tensor, token_index: ?Tensor) Tensor {
-        const embeds = embed(self.embed_tokens, tokens, token_index);
-
+    pub fn forward(self: ClipTextTransformer, tokens: Tensor) Tensor {
+        const embeds = zml.call(self.embeddings, .forward, .{tokens}).withPartialTags(.{ .s, .d });
         var hidden = embeds;
-        for (self.layers, 0..) |layer, i| {
-            _ = i; // autofix
-            hidden = zml.call(layer, .forward, .{
-                hidden,
-                token_index,
-            });
+        for (self.encoder.layers) |layer| {
+            hidden = zml.call(layer, .forward, .{hidden});
+            // TODO: tags seem to be lost by `callFunc`.
             hidden = hidden.withPartialTags(.{ .s, .d });
         }
-        // TODO: tags seem to be lost by `callFunc`.
-        const output = zml.call(self.norm, .forward, .{hidden.withPartialTags(.{ .s, .d })});
-
-        return output;
-    }
-
-    pub fn embed(embed_tokens_: zml.nn.TokenEmbedding, tokens_: Tensor, token_index: ?Tensor) Tensor {
-        const tokens = if (token_index) |idx|
-            tokens_.dynamicSlice1d(-1, 1, idx)
-        else
-            tokens_;
-        return zml.call(embed_tokens_, .forward, .{tokens}).withPartialTags(.{ .s, .d });
+        return zml.call(self.final_layer_norm, .forward, .{hidden});
     }
 };
 
@@ -162,22 +58,32 @@ pub const TransformerLayer = struct {
 
     pub fn forward(
         self: TransformerLayer,
-        x0: Tensor,
-        token_index: ?Tensor,
+        input: Tensor,
     ) Tensor {
         // Self Attention
-        //log.debug("TransformerLayer({}) -> {}", .{ x0, self.input_layernorm.forward(x0) });
-        meta.assert(x0.rank() >= 2 and x0.shape().hasTags(.{ .s, .d }), "TransformerLayer expected input shape: {{..., .s, .d}}, received: {}", .{x0});
+        const x0 = if (input.shape().isFullyTagged()) input else input.withPartialTags(.{ .b, .s, .d });
 
-        const x0_normalized = zml.call(self.input_layernorm, .forward, .{x0});
-        const delta0 = zml.call(self.self_attn, .forward, .{ x0_normalized, token_index });
+        const x0_normalized = zml.call(self.layer_norm1, .forward, .{x0});
+        const delta0 = zml.call(self.self_attn, .forward, .{x0_normalized});
         const x1 = x0.add(delta0);
 
         // Fully Connected
-        const x1_normalized = zml.call(self.post_attention_layernorm, .forward, .{x1});
+        const x1_normalized = zml.call(self.layer_norm2, .forward, .{x1});
         const x2 = zml.call(self.mlp, .forward, .{x1_normalized}).add(x1);
 
         return x2.reuseBuffer(x0);
+    }
+};
+
+const ClipTextEmbeddings = struct {
+    token_embedding: zml.nn.TokenEmbedding,
+    position_embedding: zml.nn.TokenEmbedding,
+    max_position_embeddings: u32,
+
+    pub fn forward(self: ClipTextEmbeddings, token_ids: Tensor) Tensor {
+        const position_ids = Tensor.arange(.{ .end = self.max_position_embeddings }, .i32).broadcastLeft(token_ids.shape());
+
+        return self.token_embedding.forward(token_ids).add(self.position_embedding.forward(position_ids));
     }
 };
 
@@ -201,9 +107,7 @@ pub const SelfAttn = struct {
     v_proj: zml.nn.Linear,
 
     out_proj: zml.nn.Linear,
-    num_heads: i64 = undefined,
-    num_kv_heads: i64 = 0,
-    rope_opts: zml.nn.RopeOpts = undefined,
+    num_heads: u32,
 
     /// Self Attention.
     ///   - If token_index is set, x is assumed to be the representation of one new token,
@@ -212,33 +116,23 @@ pub const SelfAttn = struct {
     /// since the beginning of the sequence, and kv_cache won't be read.
     /// In both case, kv_cache will be updated with the computed key and value.
     /// x: {.b, .s, .d } -> .{.b, .s, .d}
-    pub fn forward(self: SelfAttn, x: Tensor, token_index: ?Tensor) Tensor {
+    pub fn forward(self: SelfAttn, x: Tensor) Tensor {
         // log.debug("x.shape: {}", .{x.shape()});
-        const num_kv_heads = if (self.num_kv_heads > 0) self.num_kv_heads else self.num_heads;
-        var q = zml.call(self.q_proj, .forward, .{x}).splitAxis(-1, .{ .h = self.num_heads, .hd = .auto });
-        var k = zml.call(self.k_proj, .forward, .{x}).splitAxis(-1, .{ .h = num_kv_heads, .hd = .auto });
-        var v = zml.call(self.v_proj, .forward, .{x}).splitAxis(-1, .{ .h = num_kv_heads, .hd = .auto });
+        const nh = self.num_heads;
+        var q = zml.call(self.q_proj, .forward, .{x}).splitAxis(-1, .{ .h = nh, .hd = .auto });
+        var k = zml.call(self.k_proj, .forward, .{x}).splitAxis(-1, .{ .h = nh, .hd = .auto });
+        var v = zml.call(self.v_proj, .forward, .{x}).splitAxis(-1, .{ .h = nh, .hd = .auto });
         // Generate the attention mask.
-        const seq_len = k.dim(.k);
-        var attn_mask = zml.nn.causalAttnMask(.{ .q = seq_len, .k = seq_len }, x.dtype(), null);
-        var cos, var sin = zml.nn.ropeCosSin(.{ .s = seq_len, .hd = k.dim(.hd) }, x.dtype(), self.rope_opts);
-        if (token_index) |idx| {
-            // Note: in Pytorch it would be very inefficient to generate the full ropeCosSin and attn_mask matrices, then slice into it,
-            // but XLA is able to optimize this correctly.
-            attn_mask = attn_mask.dynamicSlice(.{ .q = .{ .start = idx, .len = 1 } });
-            cos = cos.dynamicSlice(.{ .s = .{ .start = idx, .len = 1 } });
-            sin = sin.dynamicSlice(.{ .s = .{ .start = idx, .len = 1 } });
-        }
+        const seq_len = k.dim(.s);
+        const attn_mask = zml.nn.causalAttnMask(.{ .q = seq_len, .k = seq_len }, x.dtype(), null);
 
-        // In self-attention, .s axis is used both for keys and queries.
-        q = zml.nn.rope(q, .{ cos, sin }, self.rope_opts);
-        k = zml.nn.rope(k, .{ cos, sin }, self.rope_opts);
         q = q.rename(.{ .s = .q });
         k = k.rename(.{ .s = .k });
         v = v.rename(.{ .s = .k });
 
+        log.info("q={}, k={}, v={}", .{ q, k, v });
         const attn_output = zml.nn.sdpa(q, k, v, .{ .attn_mask = attn_mask, .allow_cudnn = false });
         const attn = attn_output.merge(.{ .d = .{ .h, .hd } }).rename(.{ .q = .s });
-        return .{zml.call(self.o_proj, .forward, .{attn})};
+        return zml.call(self.out_proj, .forward, .{attn});
     }
 };
