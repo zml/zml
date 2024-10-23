29dc891702b385970e1a762524f2307c8e31bfd1
Corentin Godeau <corentin.godeau@zml.ai>
2024-10-23 14:05:18 +0200

    misc: improved neovim LSP setup script

--- examples/.nvim.lua
+++ examples/.nvim.lua
@@ -1,14 +1,22 @@
-local f, err = loadfile('.nvim.local.lua')
+-- Get the current working directory (CWD)
+local cwd = vim.fn.getcwd()
+
+-- Load the local config file if it exists
+local f, err = loadfile(".nvim.local.lua")
 if not err then
-    f()
-else 
-    require('lspconfig')["zls"].setup {
-        cmd = { "tools/zls.sh" },
-        settings = {
-            zls = {
-                enable_autofix = true,
-                zig_exe_path = vim.fn.getcwd() .. "/tools/zig.sh"
-            },
-        },
-    }
+	f()
+else
+	-- Prepend CWD to relative paths
+	local zls_cmd = cwd .. "/tools/zls.sh"
+	local zig_exe_path = cwd .. "/tools/zig.sh"
+
+	require("lspconfig")["zls"].setup({
+		cmd = { zls_cmd },
+		settings = {
+			zls = {
+				enable_autofix = true,
+				zig_exe_path = zig_exe_path,
+			},
+		},
+	})
 end
