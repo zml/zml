e60ebe064f71c060b6402c3028016e3f9fd2cac8
Steeve Morin <steeve@zml.ai>
2024-11-01 15:37:18 +0100

    NEURON

---
+++ runtimes/neuron/neuron.zig
@@ -0,0 +1,119 @@
+const builtin = @import("builtin");
+const asynk = @import("async");
+const pjrt = @import("pjrt");
+const c = @import("c");
+const std = @import("std");
+const runfiles = @import("runfiles");
+const bazel_builtin = @import("bazel_builtin");
+
+pub fn isEnabled() bool {
+    return @hasDecl(c, "ZML_RUNTIME_NEURON");
+}
+
+fn hasNeuronDevice() bool {
+    asynk.File.access("/dev/neuron0", .{ .mode = .read_only }) catch return false;
+    return true;
+}
+
+fn isRunningOnEC2() !bool {
+    const AmazonEC2 = "Amazon EC2";
+
+    var f = try asynk.File.open("/sys/devices/virtual/dmi/id/sys_vendor", .{ .mode = .read_only });
+    defer f.close() catch {};
+
+    var buf = [_]u8{0} ** AmazonEC2.len;
+    _ = try f.reader().readAll(&buf);
+
+    return std.mem.eql(u8, &buf, AmazonEC2);
+}
+
+fn toWchar(str: []const u8, out: []c.wchar_t) []c.wchar_t {
+    const len = c.mbstowcs(out.ptr, str.ptr, str.len);
+    return out[0..len];
+}
+
+fn pyErrorOrExit(status: c.PyStatus) void {
+    if (c.PyStatus_Exception(status) != 0) {
+        if (c.PyStatus_IsExit(status) != 0) {
+            std.process.exit(@intCast(status.exitcode));
+        }
+        c.Py_ExitStatusException(status);
+    }
+}
+
+fn initialize() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+    const allocator = arena.allocator();
+    defer arena.deinit();
+
+    {
+        var preconfig: c.PyPreConfig = undefined;
+        c.PyPreConfig_InitIsolatedConfig(&preconfig);
+        preconfig.utf8_mode = 1;
+        pyErrorOrExit(c.Py_PreInitialize(&preconfig));
+    }
+
+    var config: c.PyConfig = undefined;
+    c.PyConfig_InitIsolatedConfig(&config);
+    defer c.PyConfig_Clear(&config);
+
+    var r_ = try runfiles.Runfiles.create(.{ .allocator = allocator }) orelse return error.Unavailable;
+    const r = r_.withSourceRepo(bazel_builtin.current_repository);
+
+    var buf: [std.fs.max_path_bytes]u8 = undefined;
+    var wbuf: [std.fs.max_path_bytes]c.wchar_t = undefined;
+
+    {
+        const neuronx_cc = (try r.rlocation("zml/runtimes/neuron/neuronx-cc", &buf)).?;
+        const neuronx_cc_path = std.fs.path.dirname(neuronx_cc).?;
+        const path = std.posix.getenv("PATH") orelse "";
+        const new_path = try std.fmt.allocPrintZ(allocator, "{s}:{s}", .{ neuronx_cc_path, path });
+        _ = c.setenv("PATH", new_path.ptr, 1);
+    }
+
+    {
+        config.module_search_paths_set = 1;
+
+        const python_bootstrap_path = (try r.rlocation("zml/runtimes/neuron/python_bootstrap.txt", &buf)).?;
+        const python_bootstrap = try asynk.File.open(python_bootstrap_path, .{ .mode = .read_only });
+        defer python_bootstrap.close() catch {};
+
+        var step: usize = 0;
+        while (try python_bootstrap.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
+            const wline = toWchar(line, &wbuf);
+            pyErrorOrExit(c.PyWideStringList_Append(&config.module_search_paths, wline.ptr));
+            if (step == 0) {
+                pyErrorOrExit(c.PyConfig_SetString(&config, &config.home, wline.ptr));
+            }
+            step += 1;
+        }
+    }
+
+    pyErrorOrExit(c.Py_InitializeFromConfig(&config));
+
+    // release the GIL
+    _ = c.PyEval_SaveThread();
+}
+
+pub fn setNeuronCCFlags() void {
+    _ = c.setenv("NEURON_CC_FLAGS", "--model-type=transformer --auto-cast=none", 1);
+}
+
+pub fn load() !*const pjrt.Api {
+    if (comptime !isEnabled()) {
+        return error.Unavailable;
+    }
+    if (comptime builtin.os.tag != .linux) {
+        return error.Unavailable;
+    }
+    if (!(isRunningOnEC2() catch false)) {
+        return error.Unavailable;
+    }
+    if (!hasNeuronDevice()) {
+        return error.Unavailable;
+    }
+
+    try initialize();
+    setNeuronCCFlags();
+    return try pjrt.Api.loadFrom("libpjrt_neuron.so");
+}
