2d8f553ac047a74662b683d301ffcf9ea5391a3a
Steeve Morin <steeve@zml.ai>
2024-11-22 15:42:09 +0100

    stdx: add io.zig

--- stdx/stdx.zig
+++ stdx/stdx.zig
@@ -1,3 +1,4 @@
+pub const debug = @import("debug.zig");
+pub const io = @import("io.zig");
 pub const math = @import("math.zig");
 pub const meta = @import("meta.zig");
-pub const debug = @import("debug.zig");
