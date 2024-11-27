2f70fa4a0086148e7e2ac8ac7714f59b55e76507
Hugo Mano <hugo@zml.ai>
2024-11-27 17:21:34 +0100

    workspace: log message before return NotFound  (#107)
    This allow us to find which NotFound is raised

--- zml/context.zig
+++ zml/context.zig
@@ -98,7 +98,11 @@ pub const Context = struct {
                 num_platforms += 1;
             }
         }
-        if (num_platforms == 0) return error.NotFound;
+        if (num_platforms == 0) {
+            log.err("No platform available", .{});
+            return error.NoPlatformAvailable;
+        }
+
         return .{
             .platforms = platforms,
         };
