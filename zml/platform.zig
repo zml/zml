8a86c49b6237ec33f88664e19b7d5b807ed8fbda
Guillaume Wenzek <wzk@zml.ai>
2024-12-10 11:30:12 +0100

    make CreateOptions platform specific

--- zml/platform.zig
+++ zml/platform.zig
@@ -29,10 +29,11 @@ pub const Platform = struct {
     compilation_options: CompilationOptions = .{},
 
     pub const MAX_NUM_DEVICES: u8 = 32;
-    pub const CreateOptions = pjrt.Client.CreateOptions;
+    pub const CreateOptions = _CreateOptions;
 
     pub fn init(target: Target, api: *const pjrt.Api, options: CreateOptions) !Platform {
-        const pjrt_client = try pjrt.Client.init(api, options);
+        var named_values_buf: [16]pjrt.NamedValue = undefined;
+        const pjrt_client = try pjrt.Client.init(api, options.toNamedValues(target, &named_values_buf));
         const true_num_devices = pjrt_client.getAddressableDevices(api).len;
         if (true_num_devices > MAX_NUM_DEVICES) {
             log.warn("platform {} got {} devices, but ZML only support up to {} devices. Some devices won't be used.", .{ target, true_num_devices, MAX_NUM_DEVICES });
@@ -85,3 +86,59 @@ pub const Platform = struct {
         return self.pjrt_client.getProfiler(self.pjrt_api, options);
     }
 };
+
+const _CreateOptions = struct {
+    cpu: void = {},
+    cuda: ?Cuda = null,
+    rocm: void = {},
+    tpu: void = {},
+    neuron: void = {},
+
+    pub const Cuda = struct {
+        allocator: Allocator = .default,
+        // preallocate and memory fraction are only used by the bfc allocator
+        preallocate: bool = true,
+        memory_fraction: ?f32 = null,
+        collective_memory_size_mb: ?u16 = null,
+        // TODO support all of https://github.com/openxla/xla/blob/3d31c48c719d331d432132b3e0c2c5ce52650675/xla/pjrt/c/pjrt_c_api_gpu_internal.cc#L76-L86
+
+        pub const Allocator = enum {
+            default, // the client chose the best option
+            bfc, // "Best-Fit with Coalescing" algorithm
+            cuda_async, // use cudaMallocAsync
+            platform, // the platform default eg cuMalloc
+        };
+
+        pub fn writeNamedValues(self: Cuda, values: *std.ArrayListUnmanaged(pjrt.NamedValue)) void {
+            values.appendAssumeCapacity(pjrt.NamedValue.from("allocator", self.allocator));
+            values.appendAssumeCapacity(pjrt.NamedValue.from("preallocate", self.preallocate));
+            if (self.memory_fraction) |memory_fraction| {
+                values.appendAssumeCapacity(pjrt.NamedValue.from("memory_fraction", memory_fraction));
+            }
+            if (self.collective_memory_size_mb) |collective_memory_size_mb| {
+                const collective = @as(i64, collective_memory_size_mb) * 1024 * 1024;
+                values.appendAssumeCapacity(pjrt.NamedValue.from("collective_memory_size", collective));
+            }
+        }
+    };
+
+    pub fn toNamedValues(self: _CreateOptions, target: Target, out: []pjrt.NamedValue) []pjrt.NamedValue {
+        var values = std.ArrayListUnmanaged(pjrt.NamedValue).fromOwnedSlice(out);
+        values.shrinkRetainingCapacity(0);
+        switch (target) {
+            inline else => |t| {
+                const options = @field(self, @tagName(t));
+                if (@TypeOf(options) == void) return &.{};
+
+                if (options) |opt| opt.writeNamedValues(&values);
+            },
+        }
+        return values.items;
+    }
+};
+
+comptime {
+    for (std.meta.fields(Target)) |target_field| {
+        stdx.debug.assertComptime(@hasField(_CreateOptions, target_field.name), "CreateOptions doesn't list target {s}", target_field.name);
+    }
+}
