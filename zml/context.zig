zml/zml

commit d669bdc6bf11306c5eb67ddd82c0bdbb9643bbd9

zml: cleanup module.zig (#115)
* compileInternal now works with function not with "module"
* BaseExe now allocates all the memory needed to call `pjrtExecute`
* `Exe` is the type variant of `BaseExe` and enforce the right Zig arg
are passed to `call`. `exe.prepare(model)` don't need to allocate
anymore, the allocation happened during compilation.
* public entry points like `compile`, `compileModel`, `compileFn` are
now at the top of the file
* the caching logic has been mostly inlined inside `compileInternal`
* `compile`, `compileFn`, `Exe`, `BaseExe` which are the user visible
API have been moved to `exe.zig`
* `CompilationContext` and all the compilation logic stays in
`module.zig`

zml/exe.zig
@@ -1,0 +1,356 @@
+const std = @import("std");
+const stdx = @import("stdx");
+
+const aio = @import("aio.zig");
+const meta = @import("meta.zig");
+const pjrt = @import("pjrtx.zig");
+
+const Buffer = @import("buffer.zig").Buffer;
+const Bufferized = @import("tensor.zig").Bufferized;
+const CompilationContext = @import("module.zig").CompilationContext;
+const Platform = @import("platform.zig").Platform;
+const Shape = @import("shape.zig").Shape;
+const ShapeOf = @import("tensor.zig").ShapeOf;
+
+const log = std.log.scoped(.zml);
+
+test {
+    std.testing.refAllDecls(@This());
+}
+
+/// Compiles a Model struct with the given configuration and shapes, for the given platform.
+/// The steps are:
+/// * lookup at tensors available in the store and create a `model: Model` struct with them
+/// * call `model.init(init_args)` to fields of the model that aren't Tensor, ie hyperparemeters/config
+/// * generate MLIR by calling `model.forward` with tensor of the given shapes and other arguments
+pub fn compile(
+    allocator: std.mem.Allocator,
+    comptime func: anytype,
+    init_args: anytype,
+    args_shapes: ShapeOf(ModuleSignature(func).ArgsT),
+    buffer_store: aio.BufferStore,
+    platform: Platform,
+) !FnExe(func) {
+    const ModelT = ModuleSignature(func).ModelT;
+
+    var arena_state = std.heap.ArenaAllocator.init(allocator);
+    defer arena_state.deinit();
+    const arena = arena_state.allocator();
+    var model = try aio.populateModel(ModelT, arena, buffer_store);
+
+    // If the Model has a "init" function, call it with the given parameters.
+    if (@hasDecl(ModelT, "init")) {
+        // TODO(Corentin,@Improvement): Add a warning/error if there is no init function but init_args is non-void.
+        @call(.auto, ModelT.init, .{@as(*ModelT, &model)} ++ init_args);
+    }
+
+    return compileModel(allocator, func, model, args_shapes, platform);
+}
+
+/// Compiles a Model struct with the given configuration and shapes, for the given platform.
+/// Generate MLIR by calling `model.forward` with tensor of the given shapes and other arguments
+pub fn compileModel(
+    allocator: std.mem.Allocator,
+    comptime func: anytype,
+    model: ModuleSignature(func).ModelT,
+    args_shapes: ShapeOf(ModuleSignature(func).ArgsT),
+    platform: Platform,
+) !FnExe(func) {
+    const ModelT = ModuleSignature(func).ModelT;
+    const name = @typeName(ModelT) ++ ".forward";
+    log.info("Compiling {s} with {}", .{ name, args_shapes });
+
+    var context = try CompilationContext.init(allocator, name, platform);
+    defer context.deinit();
+
+    return .{ .inner = try context.compileInternal(allocator, func, .{model} ++ args_shapes) };
+}
+
+/// Compiles a function with the given configuration and shapes, for the given platform.
+/// Generate MLIR by calling the given function with tensor of the given shapes.
+pub fn compileFn(
+    allocator: std.mem.Allocator,
+    comptime func: anytype,
+    args: ShapeOf(stdx.meta.FnArgs(func)),
+    platform: Platform,
+) !FnExe(func) {
+    const name = @typeName(@TypeOf(func));
+    var context = try CompilationContext.init(allocator, name, platform);
+    defer context.deinit();
+
+    return .{ .inner = try context.compileInternal(allocator, func, args) };
+}
+
+pub fn FnExe(comptime func: anytype) type {
+    return Exe(stdx.meta.FnArgs(func), stdx.meta.FnResult(func));
+}
+
+/// Represents a ZML model, compiled into a PJRT executable, and ready to call.
+/// The buffers for the model weights are saved inside the struct and will be used in `call`.
+/// You only need to pass the remaining arguments.
+/// Creating a `ModuleExe` is a two steps proccess:
+///
+/// ```
+/// const exe: zml.FnExe(MyModel.forward) = try zml.compile(allocator, MyModel.forward, init_args, model_shapes, buffer_store, platform);`
+/// const module: zml.ModuleExe(MyModel.forward) = exe.prepare(model_buffers);
+/// ```
+pub fn ModuleExe(comptime func: anytype) type {
+    const AllArgs = stdx.meta.FnArgs(func);
+    const len = @typeInfo(AllArgs).Struct.fields.len;
+    stdx.debug.assertComptime(len > 0, "ModuleExe expects a function with at least one argument where the first one is treated as the module, got {}", .{func});
+    return Exe(stdx.meta.Tail(AllArgs), stdx.meta.FnResult(func));
+}
+
+// making this a struct force all fields to be evaluted on creation,
+// which gives a better error stacktrace
+// than delaying the error to when the object fields are read.
+const Sign = struct {
+    ModelT: type,
+    ArgsT: type,
+    ReturnT: type,
+};
+
+pub fn ModuleSignature(comptime func: anytype) Sign {
+    const AllArgsT = stdx.meta.FnArgs(func);
+    const len = @typeInfo(AllArgsT).Struct.fields.len;
+    stdx.debug.assertComptime(len > 0, "ModuleExe expects a function with at least one argument where the first one is treated as the module, got {}", .{func});
+
+    return .{
+        .ModelT = stdx.meta.Head(AllArgsT),
+        .ArgsT = stdx.meta.Tail(AllArgsT),
+        .ReturnT = stdx.meta.FnResult(func),
+    };
+}
+
+/// Represents an MLIR module compiled into a PJRT executable.
+/// The BaseExe is a plain old struct and doesn't have information about Zig types.
+///
+/// It also contains pre-allocated buffers so that we can pass them to PJRT_LoadedExecutable_Execute
+/// without allocations.
+pub const BaseExe = struct {
+    /// The platform for which this module was compiled.
+    platform: Platform,
+
+    /// The PJRT executable representing the compiled module.
+    exe: *pjrt.LoadedExecutable,
+
+    /// Pre-allocated slice of buffers to use as inputs when the module is called.
+    input_per_device: []const [*]*pjrt.Buffer,
+
+    /// Pre-allocated slice of buffers to use as outputs when the module is called.
+    output_per_device: []const [*]*pjrt.Buffer,
+
+    /// Number of buffers already fed to the executable.
+    ready_buffer_count: u32,
+
+    /// Total number of buffers needed by this executable.
+    input_buffer_count: u32,
+
+    result_shapes: []Shape,
+
+    /// Num devices used (>1 for sharded executable)
+    num_devices: u8,
+
+    /// Allocator backing memory
+    _arena: std.heap.ArenaAllocator,
+
+    pub fn init(parent_allocator: std.mem.Allocator, platform: Platform, exe: *pjrt.LoadedExecutable, args: struct { n_in: u32, result_shapes: []const Shape, n_devices: u8 }) !BaseExe {
+        var arena = std.heap.ArenaAllocator.init(parent_allocator);
+        errdefer arena.deinit();
+        const allocator = arena.allocator();
+        const n_out = args.result_shapes.len;
+        const n_devices = args.n_devices;
+        // Allocate once for all the *pjrt.Buffer we need to store ...
+        const all_buffers = try allocator.alloc(*pjrt.Buffer, (args.n_in + n_out) * n_devices);
+        const all_input_buffers, const all_output_buffers = splitBuffer(*pjrt.Buffer, all_buffers, .{ args.n_in * n_devices, n_out * n_devices });
+
+        // ... and once for all the [*]*pjrt.Buffer.
+        const all_per_device = try allocator.alloc([*]*pjrt.Buffer, 2 * n_devices);
+        const input_per_device, const output_per_device = splitBuffer([*]*pjrt.Buffer, all_per_device, .{ n_devices, n_devices });
+
+        for (0..n_devices) |i| {
+            input_per_device[i] = all_input_buffers[i * args.n_in ..].ptr;
+            output_per_device[i] = all_output_buffers[i * n_out ..].ptr;
+        }
+
+        return .{
+            .platform = platform,
+            .exe = exe,
+            .ready_buffer_count = 0,
+            .input_buffer_count = args.n_in,
+            .num_devices = args.n_devices,
+            .input_per_device = input_per_device,
+            .output_per_device = output_per_device,
+            .result_shapes = try allocator.dupe(Shape, args.result_shapes),
+            ._arena = arena,
+        };
+    }
+
+    pub fn deinit(self: BaseExe) void {
+        self._arena.deinit();
+    }
+
+    pub fn call(self: BaseExe) void {
+        stdx.debug.assert(self.input_buffer_count == self.ready_buffer_count, "BaseExe isn't ready to be called, expected {} buffer inputs got {}", .{ self.input_buffer_count, self.ready_buffer_count });
+        return self._unsafeCall();
+    }
+
+    pub fn _unsafeCall(self: BaseExe) void {
+        var events = [_]?*pjrt.Event{null} ** Platform.MAX_NUM_DEVICES;
+        const sharding = self.platform.sharding();
+
+        self.exe.execute(self.platform.pjrt_api, .{
+            .arguments = self.input_per_device,
+            .num_args = self.input_buffer_count,
+            .results = self.output_per_device,
+            .events = events[0..sharding.num_partitions],
+            // this allows to tell a specific buffer shouldn't be donated,
+            // even if it has been marked as "can be donated" during compilation.
+            // TODO: expose it ?
+            .non_donatable_input_indices = &.{},
+        }) catch unreachable;
+
+        for (events[0..sharding.num_partitions]) |e| {
+            if (e) |ev| {
+                ev.await_(self.platform.pjrt_api) catch unreachable;
+            }
+        }
+    }
+
+    pub fn serialize(self: BaseExe, writer: anytype) !void {
+        var executable = try self.exe.getExecutable(self.platform.pjrt_api);
+        var serialize_result = try executable.serialize(self.platform.pjrt_api);
+        defer serialize_result.deinit();
+        try writer.writeAll(serialize_result.bytes);
+    }
+
+    // pub fn deserialize(allocator: std.mem.Allocator, platform: Platform, reader: anytype) !Self {
+    //     const bytes = try reader.readToEndAlloc(allocator, max_pjrt_executable_size);
+    //     defer allocator.free(bytes);
+    //     return platform.pjrt_client.deserializeAndLoad(platform.pjrt_api, bytes);
+    // }
+
+    pub fn prepare(self: *BaseExe, x: anytype) void {
+        const n = fillBuffers(&x, self.input_per_device, self.ready_buffer_count);
+        self.ready_buffer_count += n;
+    }
+
+    pub fn getOutputBuffer(self: BaseExe, i: usize) Buffer {
+        var shards: Buffer.Shards = .{};
+        for (self.output_per_device) |dev_out| {
+            shards.appendAssumeCapacity(dev_out[i]);
+        }
+
+        const out_shape = self.inner.result_buffer_shapes[i];
+        return Buffer.fromPjrtBuffers(self.platform(), out_shape, shards.constSlice());
+    }
+};
+
+/// Represents a ZML function, compiled into a PJRT executable.
+/// The signature of the Exe reflects the arguments that are needed for `call`.
+pub fn Exe(ArgsT: type, ReturnT: type) type {
+    return struct {
+        const Self = @This();
+
+        /// The raw untyped compiled module.
+        inner: BaseExe,
+
+        pub fn deinit(self: Self) void {
+            self.inner.deinit();
+        }
+
+        /// Hardcode the first argument of the function to the given buffers.
+        /// Returns an Exe with one less argument in `call`.
+        /// In functional languages this is known as partial application.
+        ///
+        /// **Warning:** the new Exe reuses the underlying memory of the previous one.
+        /// The caller is responsible to come up with a strategy to call `deinit` exactly once.
+        pub fn prepare(self: Self, first_arg: Bufferized(stdx.meta.Head(ArgsT))) Exe(stdx.meta.Tail(ArgsT), ReturnT) {
+            var new: Exe(stdx.meta.Tail(ArgsT), ReturnT) = .{ .inner = self.inner };
+            new.inner.prepare(first_arg);
+            return new;
+        }
+
+        pub fn serialize(self: Self, writer: anytype) !void {
+            return try self.inner.serialize(writer);
+        }
+
+        pub fn platform(self: Self) Platform {
+            return self.inner.platform;
+        }
+
+        pub fn call(self: Self, args: Bufferized(ArgsT)) Bufferized(ReturnT) {
+            const total_ready = fillBuffers(&args, self.inner.input_per_device, self.inner.ready_buffer_count);
+            std.debug.assert(total_ready == self.inner.input_buffer_count);
+            self.inner._unsafeCall();
+            var result: Bufferized(ReturnT) = undefined;
+            assignRawBuffers(&result, self.inner.platform, self.inner.output_per_device, self.inner.result_shapes);
+            return result;
+        }
+    };
+}
+
+fn splitBuffer(T: type, buffer: []T, lengths: anytype) [lengths.len][]T {
+    var res: [lengths.len][]T = undefined;
+    var i: usize = 0;
+    inline for (&res, lengths) |*r, len| {
+        r.* = buffer[i .. i + len];
+        i += len;
+    }
+    std.debug.assert(i == buffer.len);
+    return res;
+}
+
+/// Visit the given struct and fill the `buffers` slice with the buffer associated with encountered Tensor.
+fn fillBuffers(v: anytype, buffers: []const [*]*pjrt.Buffer, start: u32) u32 {
+    const LocalContext = struct {
+        index: u32,
+        buffers: []const [*]*pjrt.Buffer,
+    };
+    var context: LocalContext = .{
+        .index = start,
+        .buffers = buffers,
+    };
+    meta.visit((struct {
+        fn cb(ctx: *LocalContext, buffer: *const Buffer) void {
+            // stdx.debug.assert(!buffer._data.isDeleted(), "Can't use {} (argument buffer {}) because its pjrt buffer has been donated", .{ buffer, ctx.index });
+            const model_sharding = ctx.buffers.len;
+            stdx.debug.assert(buffer._shards.len == model_sharding, "Can't feed a {}-sharded tensor into a {}-sharded model", .{ buffer._shards.len, ctx.buffers.len });
+            for (buffer._shards.constSlice(), 0..) |shard, d| {
+                ctx.buffers[d][ctx.index] = shard;
+            }
+            ctx.index += 1;
+        }
+    }).cb, &context, v);
+    return context.index;
+}
+
+/// Visit the given struct and override tensors by creating a new one using the provided PJRT buffers.
+fn assignRawBuffers(v: anytype, platform: Platform, buffers: []const [*]*pjrt.Buffer, buffer_shapes: []Shape) void {
+    const LocalContext = struct {
+        index: u32,
+        platform: Platform,
+        buffers: []const [*]*pjrt.Buffer,
+        buffer_shapes: []Shape,
+    };
+    var local_ctx: LocalContext = .{
+        .index = 0,
+        .platform = platform,
+        .buffers = buffers,
+        .buffer_shapes = buffer_shapes,
+    };
+    meta.visit((struct {
+        fn cb(ctx: *LocalContext, buffer: *Buffer) void {
+            const i = ctx.index;
+            ctx.index += 1;
+            if (i >= ctx.buffer_shapes.len) return;
+
+            var shards: Buffer.Shards = .{};
+            for (ctx.buffers) |buff| {
+                shards.appendAssumeCapacity(buff[i]);
+            }
+            buffer.* = Buffer.fromPjrtBuffers(ctx.platform, ctx.buffer_shapes[i], shards.constSlice());
+        }
+    }).cb, &local_ctx, v);
+    stdx.debug.internalAssert(local_ctx.index == buffer_shapes.len, "Pjrt call returned {} tensors, but the return type {s}, contains {} Buffers. Note that modules need to have a comptime know number of returned tensors.", .{ buffers.len, @typeName(@TypeOf(v)), local_ctx.index });
+}

zml/testing.zig
@@ -187,7 +187,7 @@ pub fn testLayerOut(
     log.info("Testing {s}", .{name});
 
     const fwd = @TypeOf(layer).forward;
-    const FwdSign = zml.module.ModuleSignature(fwd);
+    const FwdSign = zml.ModuleSignature(fwd);
 
     const input_tensors = try zml.aio.populateModelWithPrefix(FwdSign.ArgsT, alloc, activations, name ++ ".in");
     const input_shapes = try shapesOf(input_tensors, alloc);
@@ -204,7 +204,7 @@ pub fn testLayerOut(
     if (exe.inner.result_buffer_count != n_out_exp) {
         log.warn("Reference models produces {d} outputs, but implementation produces {d}", .{ n_out_exp, exe.inner.result_buffer_count });
     }
-    const mod = try exe.prepare(alloc, layer_weights);
+    const mod = exe.prepare(layer_weights);
 
     const FetchCtx = struct {
         store: zml.aio.BufferStore,

zml/zml.zig
@@ -18,6 +18,7 @@ pub const Tensor = @import("tensor.zig").Tensor;
 
 // Namespaces
 pub const context = @import("context.zig");
+pub const exe = @import("exe.zig");
 pub const floats = @import("floats.zig");
 pub const helpers = @import("helpers.zig");
 pub const nn = @import("nn.zig");
@@ -30,9 +31,11 @@ pub const torch = @import("torch.zig");
 pub const tokenizer = @import("tokenizer.zig");
 
 pub const call = ops.call;
-pub const compile = module.compile;
-pub const compileModel = module.compileModel;
-pub const compileFn = module.compileFn;
+pub const compile = exe.compile;
+pub const compileFn = exe.compileFn;
+pub const compileModel = exe.compileModel;
+pub const FnExe = exe.FnExe;
+pub const ModuleExe = exe.ModuleExe;
 
 pub const ops = @import("ops.zig");
 pub const tools = struct {

stdx/meta.zig
@@ -156,3 +156,25 @@ pub fn FnArgs(comptime func: anytype) type {
 pub fn FnResult(comptime func: anytype) type {
     return FnSignature(func, null).ReturnT;
 }
+
+pub fn Head(Tuple: type) type {
+    return switch (@typeInfo(Tuple)) {
+        .Struct => |struct_info| {
+            if (struct_info.fields.len == 0) @compileError("Can't tail empty tuple");
+            return struct_info.fields[0].type;
+        },
+        else => @compileError("Head works on tuple type"),
+    };
+}
+
+pub fn Tail(Tuple: type) type {
+    return switch (@typeInfo(Tuple)) {
+        .Struct => |struct_info| {
+            if (struct_info.fields.len == 0) @compileError("Can't tail empty tuple");
+            var types: [struct_info.fields.len - 1]type = undefined;
+            for (struct_info.fields[1..], 0..) |field, i| types[i] = field.type;
+            return std.meta.Tuple(&types);
+        },
+        else => @compileError("Tail works on tuple type"),
+    };
+}

examples/benchmark/main.zig
@@ -5,16 +5,12 @@ const flags = @import("tigerbeetle/flags");
 
 // set log level to debug to print the generated IR
 pub const std_options = .{
-    .log_level = .debug,
+    .log_level = .warn,
 };
 
-/// Model definition
-const Benchmark = struct {
-    pub fn forward(self: Benchmark, a: zml.Tensor, b: zml.Tensor) zml.Tensor {
-        _ = self;
-        return a.withSharding(.{.k}).dot(b.withSharding(.{.k}), .{.k}).withSharding(.{.m});
-    }
-};
+pub fn benchmark(a: zml.Tensor, b: zml.Tensor) zml.Tensor {
+    return a.withSharding(.{.k}).dot(b.withSharding(.{.k}), .{.k}).withSharding(.{.m});
+}
 
 pub fn main() !void {
     try asynk.AsyncThread.main(std.heap.c_allocator, asyncMain);
@@ -33,11 +29,6 @@ pub fn asyncMain() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
-
-    // Arena allocator for BufferStore etc.
-    var arena_state = std.heap.ArenaAllocator.init(allocator);
-    defer arena_state.deinit();
-    const arena = arena_state.allocator();
 
     var context = try zml.Context.init();
     defer context.deinit();
@@ -60,17 +51,14 @@ pub fn asyncMain() !void {
     // Start compiling.
     // The shape of the input tensor, we have to pass in manually.
     timer.reset();
-    var compilation = try asynk.asyncc(zml.module.compileModel, .{ allocator, Benchmark.forward, Benchmark{}, .{ a_shape, b_shape }, platform });
+    var compilation = try asynk.asyncc(zml.compileFn, .{ allocator, benchmark, .{ a_shape, b_shape }, platform });
 
     // Wait for compilation to finish
-    const compiled = try compilation.awaitt();
+    const executable = try compilation.awaitt();
+    defer executable.deinit();
     const compilation_elapsed = timer.lap() / std.time.ns_per_ms;
     std.debug.print("-" ** 160 ++ "\n\n", .{});
     std.debug.print("✅ Compiled Benchmark model in {d} milliseconds! \n", .{compilation_elapsed});
-
-    // pass the model weights to the compiled module to create an executable module
-    var executable = try compiled.prepare(arena, .{});
-    defer executable.deinit();
 
     var rng = std.Random.DefaultPrng.init(0);
     const random = rng.random();

examples/llama/main.zig
@@ -32,8 +32,8 @@ pub const std_options = .{
 
 pub fn generateText(
     llama: LlamaLM,
-    mod_prefill: zml.module.ExeWithWeights(LlamaLM.forward),
-    mod: zml.module.ExeWithWeights(LlamaLM.forward),
+    mod_prefill: zml.ModuleExe(LlamaLM.forward),
+    mod: zml.ModuleExe(LlamaLM.forward),
     tokenizer: zml.tokenizer.Tokenizer,
     allocator: std.mem.Allocator,
     seed: u128,
@@ -221,9 +221,9 @@ pub fn asyncMain() !void {
     defer zml.aio.unloadBuffers(&llama_weights);
     log.info("✅\tLoaded weights in {d}ms", .{start.read() / std.time.ns_per_ms});
 
-    var llama_module_prefill = try (try fut_mod_prefill.awaitt()).prepare(allocator, llama_weights);
+    var llama_module_prefill = (try fut_mod_prefill.awaitt()).prepare(llama_weights);
     defer llama_module_prefill.deinit();
-    var llama_module = try (try fut_mod.awaitt()).prepare(allocator, llama_weights);
+    var llama_module = (try fut_mod.awaitt()).prepare(llama_weights);
     defer llama_module.deinit();
     log.info("✅\tCompiled model in {d}ms", .{start.read() / std.time.ns_per_ms});
 

examples/mnist/mnist.zig
@@ -86,7 +86,7 @@ pub fn asyncMain() !void {
     const compiled_mnist = try compilation.awaitt();
     log.info("✅ Compiled model in {d}ms", .{start_time.read() / std.time.ns_per_ms});
 
-    var mnist = try compiled_mnist.prepare(allocator, model_weights);
+    const mnist = compiled_mnist.prepare(model_weights);
     defer mnist.deinit();
     log.info("✅ Weights transferred in {d}ms", .{start_time.read() / std.time.ns_per_ms});
 

examples/simple_layer/main.zig
@@ -74,7 +74,7 @@ pub fn asyncMain() !void {
     const compiled = try compilation.awaitt();
 
     // pass the model weights to the compiled module to create an executable module
-    var executable = try compiled.prepare(arena, model_weights);
+    var executable = compiled.prepare(model_weights);
     defer executable.deinit();
 
     // prepare an input buffer

commit 05879b41fa9a9966c99a3b63f64c6f6cb4357d72

Fix test layer (#124)
* testLayer was silently broken by #115 : test + fix so we detect this
in the future.
* mapAlloc had a regression in #115 wrt struct with 0 size fields: test
+ fix

* add Python `venv` to gitignore

.gitignore
@@ -23,7 +23,10 @@ zig-out/
 # Data files
 *.wav
 *.ecdc
+
+# Python slope
 .venv
+venv
 
 # Editor specific
 ## Neovim

zml/context.zig
@@ -6,7 +6,7 @@ const runtimes = @import("runtimes");
 const std = @import("std");
 const stdx = @import("stdx");
 
-const platform = @import("platform.zig");
+const zml_platform = @import("platform.zig");
 const pjrt = @import("pjrtx.zig");
 
 const HostBuffer = @import("hostbuffer.zig").HostBuffer;
@@ -17,6 +17,10 @@ const Target = @import("platform.zig").Target;
 
 const available_targets = @import("platform.zig").available_targets;
 const log = std.log.scoped(.@"zml/context");
+
+test {
+    std.testing.refAllDecls(Context);
+}
 
 /// Every program using ZML must start with a `zml.Context.init(.{});`
 /// The ZML context contains global state to interact with the different
@@ -149,7 +153,7 @@ pub const Context = struct {
         return platform_ orelse @panic("No platform found !");
     }
 
-    pub fn printAvailablePlatforms(self: Context, selected: platform.Platform) void {
+    pub fn printAvailablePlatforms(self: Context, selected: Platform) void {
         // List available targets
         log.info("Available Platforms:", .{});
         const selected_prefix = "✅";
@@ -157,7 +161,7 @@ pub const Context = struct {
         const selected_postfix = "(AUTO-SELECTED)";
         const not_selected_postfix = "";
 
-        for (platform.available_targets) |target| {
+        for (zml_platform.available_targets) |target| {
             log.info("  {s} {s} {s}", .{
                 if (target == selected.target) selected_prefix else not_selected_prefix,
                 @tagName(target),

zml/exe.zig
@@ -241,8 +241,7 @@ pub const BaseExe = struct {
             shards.appendAssumeCapacity(dev_out[i]);
         }
 
-        const out_shape = self.inner.result_buffer_shapes[i];
-        return Buffer.fromPjrtBuffers(self.platform(), out_shape, shards.constSlice());
+        return Buffer.fromPjrtBuffers(self.platform, self.result_shapes[i], shards.constSlice());
     }
 };
 

zml/meta.zig
@@ -120,9 +120,11 @@ pub fn mapAlloc(comptime cb: anytype, allocator: std.mem.Allocator, ctx: FnParam
         return;
     }
 
+    if (@sizeOf(ToStruct) == 0) return;
+
     switch (type_info_to) {
         .Struct => |info| inline for (info.fields) |field| {
-            // if (field.is_comptime) continue;
+            if (field.is_comptime or @sizeOf(field.type) == 0) continue;
             const field_type_info = @typeInfo(field.type);
             // If the field is already a pointer, we recurse with it directly, otherwise, we recurse with a pointer to the field.
             switch (field_type_info) {
@@ -187,6 +189,8 @@ test mapAlloc {
         }
     };
 
+    const Empty = struct {};
+
     const AA = struct {
         field: A,
         array: [2]A,
@@ -195,6 +199,7 @@ test mapAlloc {
         // We want to allow conversion from comptime to runtime, because Zig type inference works like this.
         comptime static_val: u8 = 8,
         comptime static_slice: [2]A = .{ .{ .a = 11 }, .{ .a = 12 } },
+        field_with_empty: struct { A, Empty },
     };
     const BB = struct {
         field: B,
@@ -203,6 +208,7 @@ test mapAlloc {
         other: u8,
         static_val: u8,
         static_slice: []B,
+        field_with_empty: struct { B, Empty },
     };
 
     const aa: AA = .{
@@ -210,6 +216,7 @@ test mapAlloc {
         .array = .{ .{ .a = 5 }, .{ .a = 6 } },
         .other = 7,
         .slice = &.{ .{ .a = 9 }, .{ .a = 10 } },
+        .field_with_empty = .{ .{ .a = 9 }, .{} },
     };
     var bb: BB = undefined;
 

zml/testing.zig
@@ -201,8 +201,8 @@ pub fn testLayerOut(
     const exe = try zml.compileModel(alloc, fwd, layer, input_shapes, platform);
 
     const n_out_exp = activations.countLayers(out_name);
-    if (exe.inner.result_buffer_count != n_out_exp) {
-        log.warn("Reference models produces {d} outputs, but implementation produces {d}", .{ n_out_exp, exe.inner.result_buffer_count });
+    if (exe.inner.result_shapes.len != n_out_exp) {
+        log.warn("Reference models produces {d} outputs, but implementation produces {d}", .{ n_out_exp, exe.inner.result_shapes.len });
     }
     const mod = exe.prepare(layer_weights);
 
@@ -243,13 +243,13 @@ pub fn testLayerOut(
 
     var buf: [1024]u8 = undefined;
     var failed: bool = false;
-    for (0..mod.inner.result_buffer_count) |i| {
+    for (0..mod.inner.result_shapes.len) |i| {
         const full_name = std.fmt.bufPrint(&buf, "{s}.{d}", .{ out_name, i }) catch unreachable;
         const expected_out = activations.get(full_name) orelse {
             log.warn("Output buffer not found: {s}", .{full_name});
             continue;
         };
-        zml.testing.expectClose(expected_out, mod.getOutputBuffer(i), tolerance) catch |err| switch (err) {
+        zml.testing.expectClose(expected_out, mod.inner.getOutputBuffer(i), tolerance) catch |err| switch (err) {
             error.TestUnexpectedResult => {
                 log.err("{s}.{d} doesn't match !", .{ out_name, i });
                 failed = true;
@@ -261,6 +261,34 @@ pub fn testLayerOut(
 
     if (failed) return error.TestUnexpectedResult;
     log.info("all good for {s} !", .{name});
+}
+
+test testLayer {
+    const platform = env();
+
+    // create a model
+    const layer: zml.nn.Linear = .{
+        .weight = zml.Tensor{ ._shape = zml.Shape.init(.{ 5, 2 }, .f32), ._id = .{ .buffer_id = 42 } },
+    };
+    const layer_weights: zml.Bufferized(zml.nn.Linear) = .{
+        .weight = try zml.Buffer.fromArray(
+            platform,
+            [5][2]f32{ .{ 0, 0 }, .{ 0, 1 }, .{ 1, 2 }, .{ -1, -1 }, .{ -1, 0 } },
+        ),
+    };
+
+    // create a buffer store containing the activations:
+    var activations = try zml.aio.BufferStore.init(std.testing.allocator, &.{});
+    defer activations.deinit();
+    {
+        const input = zml.HostBuffer.fromArray(&[2]f32{ 1, -1 });
+        try activations.buffers.put(activations.arena.allocator(), "model.layer.in.0", input);
+        const output = zml.HostBuffer.fromArray(&[5]f32{ 0, -1, -1, 0, -1 });
+        try activations.buffers.put(activations.arena.allocator(), "model.layer.out.0", output);
+    }
+
+    // test the ZML layer reproduces the "captured" activations:
+    try zml.testing.testLayer(platform, activations, "model.layer", layer, layer_weights, 1e-5);
 }
 
 pub inline fn expectEqual(expected: anytype, actual: @TypeOf(expected)) !void {

zml/zml.zig
@@ -36,6 +36,7 @@ pub const compileFn = exe.compileFn;
 pub const compileModel = exe.compileModel;
 pub const FnExe = exe.FnExe;
 pub const ModuleExe = exe.ModuleExe;
+pub const ModuleSignature = exe.ModuleSignature;
 
 pub const ops = @import("ops.zig");
 pub const tools = struct {