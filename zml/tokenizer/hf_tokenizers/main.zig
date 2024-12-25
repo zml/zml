2fedeaaa81ca40e63625a9259cb307c4061e1d44
Steeve Morin <steeve@zml.ai>
2024-12-25 23:38:43 +0100

    WIP

--- zml/tokenizer/hf_tokenizers/main.zig
+++ zml/tokenizer/hf_tokenizers/main.zig
@@ -2,6 +2,21 @@ const std = @import("std");
 const c = @import("c");
 
 pub const HFTokenizers = opaque {
+    pub const Encoded = struct {
+        ids: []const u32,
+
+        pub fn deinit(self: *Encoded) void {
+            c.hf_tokenizers_tokens_drop(c.zig_slice{ .ptr = @ptrCast(@constCast(self.ids.ptr)), .len = @intCast(self.ids.len) });
+        }
+    };
+
+    pub const Decoded = struct {
+        str: []const u8,
+
+        pub fn deinit(self: *Decoded) void {
+            c.hf_tokenizers_str_drop(c.zig_slice{ .ptr = @ptrCast(@constCast(self.str.ptr)), .len = @intCast(self.str.len) });
+        }
+    };
     pub fn init(model: []const u8) *HFTokenizers {
         return @ptrCast(c.hf_tokenizers_new(c.zig_slice{ .ptr = @constCast(model.ptr), .len = @intCast(model.len) }));
     }
@@ -10,9 +25,18 @@ pub const HFTokenizers = opaque {
         return c.hf_tokenizers_drop(self);
     }
 
-    pub fn encode(self: *HFTokenizers, input: []const u8) []i32 {
+    pub fn encode(self: *HFTokenizers, input: []const u8) Encoded {
         const ret = c.hf_tokenizers_encode(self, c.zig_slice{ .ptr = @constCast(input.ptr), .len = @intCast(input.len) });
-        return @as([*c]i32, @alignCast(@ptrCast(ret.ptr)))[0..ret.len];
+        return .{
+            .ids = @as([*c]const u32, @alignCast(@ptrCast(ret.ptr)))[0..ret.len]
+        };
+    }
+
+    pub fn decode(self: *HFTokenizers, input: []const u32) Decoded {
+        const ret = c.hf_tokenizers_decode(self, c.zig_slice{ .ptr = @ptrCast(@constCast(input.ptr)), .len = @intCast(input.len) });
+        return .{
+            .str = @as([*c]const u8, @alignCast(@ptrCast(ret.ptr)))[0..ret.len]
+        };
     }
 };
 
@@ -28,6 +52,22 @@ pub fn main() !void {
     defer HFTokenizers.deinit(tokenizer);
 
     const input = "Hello, world! plane pouet plane";
-    const encoded = HFTokenizers.encode(tokenizer, input);
-    std.debug.print("{any} {any}\n", .{tokenizer, encoded});
+    var encoded = HFTokenizers.encode(tokenizer, input);
+    defer encoded.deinit();
+    var pouet = std.ArrayList(u32).init(std.heap.c_allocator);
+    defer pouet.deinit();
+
+    // try pouet.appendSlice(encoded.ids);
+
+    var t = try std.time.Timer.start();
+    for (0..100) |i| {
+        _ = i; // autofix
+        try pouet.appendSlice(encoded.ids);
+        t.reset();
+        var decoded = HFTokenizers.decode(tokenizer, pouet.items);
+        defer decoded.deinit();
+        const elapsed = t.lap();
+        // std.debug.print("{any} {any} {d}us\n", .{tokenizer, encoded, elapsed / std.time.ns_per_us});
+        std.debug.print("{any} {any} {s} {d}ns {d}us\n", .{tokenizer, encoded, decoded.str, elapsed, elapsed / std.time.ns_per_us});
+    }
 }
