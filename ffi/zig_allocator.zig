0852ad8c972506470905047ce9c164018bdbe755
Steeve Morin <steeve@zml.ai>
2024-12-29 16:29:54 +0000

    WIP

---
+++ ffi/zig_allocator.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+const c = @import("c");
+
+pub const ZigAllocator = struct {
+    pub inline fn from(allocator: std.mem.Allocator) c.zig_allocator {
+        return .{
+            .ctx = @ptrCast(@alignCast(&allocator)),
+            .alloc = &alloc,
+            .free = &free,
+        };
+    }
+
+    pub fn alloc(ctx: ?*const anyopaque, elem: usize, nelems: usize, alignment: usize) callconv(.C) ?*anyopaque {
+        const self: *const std.mem.Allocator = @ptrCast(@alignCast(ctx));
+        const ret = self.rawAlloc(elem * nelems, std.math.log2_int(usize, alignment), @returnAddress()) orelse return null;
+        return @ptrCast(ret);
+    }
+
+    pub fn free(ctx: ?*const anyopaque, ptr: ?*anyopaque, elem: usize, nelems: usize, alignment: usize) callconv(.C) void {
+        const self: *const std.mem.Allocator = @ptrCast(@alignCast(ctx));
+        const memory: [*c]u8 = @ptrCast(ptr);
+        const size = elem * nelems;
+        self.rawFree(memory[0..size], std.math.log2_int(usize, alignment), @returnAddress());
+    }
+};
---
+++ ffi/zig_slice.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdlib.h>
+
+typedef struct
+{
+    void *ptr;
+    size_t len;
+} zig_slice;
