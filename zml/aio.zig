842787343a4983c507f5c15bb4d0c913033a5750
Steeve Morin <steeve@zml.ai>
2025-01-07 17:30:13 +0100

    WIP

--- zml/aio.zig
+++ zml/aio.zig
@@ -445,6 +445,7 @@ fn _populateStruct(
             return true;
         },
         .Void => true,
+        .Union => true,
         else => if (required) {
             log.err("{s}: {s} type not supported", .{ prefix, @typeName(T) });
             return error.UnsupportedMetadataType;
