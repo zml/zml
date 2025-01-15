c540ea7c0a5fe95a34ad7016afc8f4a6f6e29b57
Steeve Morin <steeve@zml.ai>
2025-01-15 10:04:13 +0000

    runtimes/cuda,zml: setup xla_gpu_cuda_data_dir via environment variable
    This is the way it should be [1]. Thankfully, other plugins don't use it.
    
    1. https://github.com/openxla/xla/issues/21428

--- runtimes/cuda/cuda.zig
+++ runtimes/cuda/cuda.zig
@@ -1,7 +1,12 @@
-const builtin = @import("builtin");
+const std = @import("std");
+
 const asynk = @import("async");
-const pjrt = @import("pjrt");
+const bazel_builtin = @import("bazel_builtin");
+const builtin = @import("builtin");
 const c = @import("c");
+const pjrt = @import("pjrt");
+const runfiles = @import("runfiles");
+const stdx = @import("stdx");
 
 pub fn isEnabled() bool {
     return @hasDecl(c, "ZML_RUNTIME_CUDA");
@@ -10,6 +15,23 @@ pub fn isEnabled() bool {
 fn hasNvidiaDevice() bool {
     asynk.File.access("/dev/nvidiactl", .{ .mode = .read_only }) catch return false;
     return true;
+}
+
+fn setupXlaGpuCudaDirFlag() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+    defer arena.deinit();
+
+    var r_ = try runfiles.Runfiles.create(.{ .allocator = arena.allocator() }) orelse {
+        stdx.debug.panic("Unable to find CUDA directory", .{});
+    };
+
+    const source_repo = bazel_builtin.current_repository;
+    const r = r_.withSourceRepo(source_repo);
+    const cuda_data_dir = (try r.rlocationAlloc(arena.allocator(), "libpjrt_cuda/sandbox")).?;
+    const xla_flags = std.process.getEnvVarOwned(arena.allocator(), "XLA_FLAGS") catch "";
+    const new_xla_flagsZ = try std.fmt.allocPrintZ(arena.allocator(), "--xla_gpu_cuda_data_dir={s} {s}", .{ cuda_data_dir, xla_flags });
+
+    _ = c.setenv("XLA_FLAGS", new_xla_flagsZ, 1);
 }
 
 pub fn load() !*const pjrt.Api {
@@ -22,6 +44,10 @@ pub fn load() !*const pjrt.Api {
     if (!hasNvidiaDevice()) {
         return error.Unavailable;
     }
+
+    // CUDA path has to be set _before_ loading the PJRT plugin.
+    // See https://github.com/openxla/xla/issues/21428
+    try setupXlaGpuCudaDirFlag();
 
     return try pjrt.Api.loadFrom("libpjrt_cuda.so");
 }
