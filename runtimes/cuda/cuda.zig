a9e17d35fd7dd41f21dd1063f3eb2234cb5e9d52
Steeve Morin <steeve@zml.ai>
2025-01-24 19:42:22 +0100

    runtimes/cuda: downgrade the PJRT plugin back to 0.4.38 (#164)
    There is a performance regression on XLA master and 0.5.0.
    
    Get back to 0.4.38 for now.

--- runtimes/cuda/cuda.zig
+++ runtimes/cuda/cuda.zig
@@ -1,10 +1,16 @@
 const builtin = @import("builtin");
 const std = @import("std");
-const asynk = @import("async");
-const pjrt = @import("pjrt");
-const c = @import("c");
 
-const nvidiaLibsPath = "/usr/local/cuda/lib64";
+const asynk = @import("async");
+const bazel_builtin = @import("bazel_builtin");
+const c = @import("c");
+const pjrt = @import("pjrt");
+const runfiles = @import("runfiles");
+const stdx = @import("stdx");
+
+const nvidiaLibsPath = "/cuda/";
+
+const log = std.log.scoped(.@"zml/runtime/cuda");
 
 pub fn isEnabled() bool {
     return @hasDecl(c, "ZML_RUNTIME_CUDA");
@@ -22,7 +28,24 @@ fn hasCudaPathInLDPath() bool {
         return false;
     }
 
-    return std.mem.indexOf(u8, std.mem.span(ldLibraryPath), nvidiaLibsPath) != null;
+    return std.ascii.indexOfIgnoreCase(std.mem.span(ldLibraryPath), nvidiaLibsPath) != null;
+}
+
+fn setupXlaGpuCudaDirFlag() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+    defer arena.deinit();
+
+    var r_ = try runfiles.Runfiles.create(.{ .allocator = arena.allocator() }) orelse {
+        stdx.debug.panic("Unable to find CUDA directory", .{});
+    };
+
+    const source_repo = bazel_builtin.current_repository;
+    const r = r_.withSourceRepo(source_repo);
+    const cuda_data_dir = (try r.rlocationAlloc(arena.allocator(), "libpjrt_cuda/sandbox")).?;
+    const xla_flags = std.process.getEnvVarOwned(arena.allocator(), "XLA_FLAGS") catch "";
+    const new_xla_flagsZ = try std.fmt.allocPrintZ(arena.allocator(), "{s} --xla_gpu_cuda_data_dir={s}", .{ xla_flags, cuda_data_dir });
+
+    _ = c.setenv("XLA_FLAGS", new_xla_flagsZ, 1);
 }
 
 pub fn load() !*const pjrt.Api {
@@ -36,8 +59,12 @@ pub fn load() !*const pjrt.Api {
         return error.Unavailable;
     }
     if (hasCudaPathInLDPath()) {
-        std.log.warn("Detected {s} in LD_LIBRARY_PATH. This can lead to undefined behaviors and crashes", .{nvidiaLibsPath});
+        log.warn("Detected {s} in LD_LIBRARY_PATH. This can lead to undefined behaviors and crashes", .{nvidiaLibsPath});
     }
+
+    // CUDA path has to be set _before_ loading the PJRT plugin.
+    // See https://github.com/openxla/xla/issues/21428
+    try setupXlaGpuCudaDirFlag();
 
     return try asynk.callBlocking(pjrt.Api.loadFrom, .{"libpjrt_cuda.so"});
 }
