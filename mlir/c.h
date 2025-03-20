bf4e4c34b49d1289c3d30d89acdfd988d8dcd508
Guillaume Wenzek <wzk@zml.ai>
2025-03-20 15:06:05 +0100

    add scf

--- mlir/c.h
+++ mlir/c.h
@@ -3,6 +3,7 @@
 #include <mlir-c/Dialect/Arith.h>
 #include <mlir-c/Dialect/Func.h>
 #include <mlir-c/Dialect/Math.h>
+#include <mlir-c/Dialect/SCF.h>
 #include <mlir-c/IR.h>
 #include <mlir-c/Pass.h>
 #include <mlir-c/Transforms.h>
