b2f7c7ccdc7a44abb314afbc50a2bb227310d5f8
Corentin Kerisit <corentin.kerisit@gmail.com>
2025-07-15 17:18:29 +0200

    renaming

--- runtimes/cpu/cpu.zig
+++ runtimes/cpu/cpu.zig
@@ -27,9 +27,7 @@ pub fn load() !*const pjrt.Api {
     };
     const source_repo = bazel_builtin.current_repository;
     const r = r_.withSourceRepo(source_repo);
-    const cuda_data_dir = (try r.rlocationAlloc(arena.allocator(), "zml/runtimes/cpu/sandbox/lib")).?;
-
-    std.debug.print("Loading CPU PJRT API from: {s}\n", .{cuda_data_dir});
+    const cpu_sandbox_dir = (try r.rlocationAlloc(arena.allocator(), "zml/runtimes/cpu/sandbox/lib")).?;
 
     const ext = switch (builtin.os.tag) {
         .windows => ".dll",
@@ -37,7 +35,7 @@ pub fn load() !*const pjrt.Api {
         else => ".so",
     };
 
-    const library = try std.fmt.allocPrintZ(arena.allocator(), "{s}/libpjrt_cpu{s}", .{ cuda_data_dir, ext });
+    const library = try std.fmt.allocPrintZ(arena.allocator(), "{s}/libpjrt_cpu{s}", .{ cpu_sandbox_dir, ext });
 
     return try asynk.callBlocking(pjrt.Api.loadFrom, .{library});
 }
