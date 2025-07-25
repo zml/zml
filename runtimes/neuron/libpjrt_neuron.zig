9a86402c2b3a82f06c6629863431ab2c1599f85d
Corentin Kerisit <corentin.kerisit@gmail.com>
2025-07-25 18:55:33 +0000

    WIP

--- runtimes/neuron/libpjrt_neuron.zig
+++ runtimes/neuron/libpjrt_neuron.zig
@@ -7,6 +7,9 @@ const stdx = @import("stdx");
 
 const log = std.log.scoped(.@"zml/runtimes/neuron");
 
+var api_inner: *c.PJRT_Api = undefined;
+var api_proxy: c.PJRT_Api = undefined;
+
 fn comptimeStrJoin(comptime separator: [:0]const u8, comptime slices: []const [:0]const u8) [:0]const u8 {
     comptime var ret = slices[0];
     inline for (slices[1..]) |slice| {
@@ -95,13 +98,12 @@ fn setupPythonEnv(sandbox_path: []const u8) !void {
 fn setupBinPath(allocator: std.mem.Allocator, sandbox_path: []const u8) !void {
     var buf: [std.fs.max_path_bytes]u8 = undefined;
     const bin_path = try stdx.fs.path.bufJoin(&buf, &.{ sandbox_path, "bin" });
-    std.debug.print(">>>>>>>>> {s}\n\n", .{bin_path});
     const old_path = std.posix.getenv("PATH") orelse "";
     const new_path = try std.fmt.allocPrintZ(allocator, "{s}:{s}", .{ bin_path, old_path });
     _ = c.setenv("PATH", new_path, 1);
 }
 
-pub export fn GetPjrtApi() ?*anyopaque {
+pub export fn GetPjrtApi() ?*c.PJRT_Api {
     var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
     defer arena.deinit();
 
@@ -129,23 +131,51 @@ pub export fn GetPjrtApi() ?*anyopaque {
         };
     }
 
-    const GetPjrtApi_inner = blk: {
-        var lib_path_buf: [std.fs.max_path_bytes]u8 = undefined;
-        const library = stdx.fs.path.bufJoinZ(&lib_path_buf, &.{ sandbox_path, "lib", "libneuronpjrt.so" }) catch unreachable;
+    api_inner = blk: {
+        const GetPjrtApi_inner = GetPjrtApi_blk: {
+            var lib: std.DynLib = .{
+                .inner = .{
+                    .handle = handle_blk: {
+                        var lib_path_buf: [std.fs.max_path_bytes]u8 = undefined;
+                        const library = stdx.fs.path.bufJoinZ(&lib_path_buf, &.{ sandbox_path, "lib", "libneuronpjrt.so" }) catch |err| {
+                            log.err("Failed to find NEURON PJRT library: {any}", .{err});
+                            return null;
+                        };
+                        break :handle_blk std.c.dlopen(library, .{ .LAZY = true, .GLOBAL = false, .NODELETE = true }) orelse {
+                            log.err("Unable to dlopen plugin: {?s}", .{std.mem.span(std.c.dlerror())});
+                            return null;
+                        };
+                    },
+                },
+            };
 
-        var lib: std.DynLib = blk_lib: {
-            const handle = std.c.dlopen(library, .{ .LAZY = true, .GLOBAL = true, .NODELETE = true }) orelse {
-                log.err("Unable to dlopen plugin: {s}", .{library});
+            break :GetPjrtApi_blk lib.lookup(*const fn () callconv(.c) *c.PJRT_Api, "GetPjrtApi") orelse {
+                log.err("Unable to find symbol GetPjrtApi in plugin: {?s}", .{std.mem.span(std.c.dlerror())});
                 return null;
             };
-            break :blk_lib .{ .inner = .{ .handle = handle } };
         };
 
-        break :blk lib.lookup(*const fn () callconv(.C) *anyopaque, "GetPjrtApi") orelse {
-            log.err("Unable to find symbol GetPjrtApi in plugin: {s}", .{library});
-            return null;
-        };
+        break :blk GetPjrtApi_inner();
     };
 
-    return GetPjrtApi_inner();
+    api_proxy = blk: {
+        var tmp: c.PJRT_Api = .{};
+        const api_struct_size = @min(@sizeOf(c.PJRT_Api), api_inner.struct_size);
+        @memcpy(
+            std.mem.asBytes(&tmp)[0..api_struct_size],
+            std.mem.asBytes(api_inner)[0..api_struct_size],
+        );
+        break :blk tmp;
+    };
+
+    // Setup the API proxy functions
+    api_proxy.PJRT_Plugin_Attributes = &struct {
+        fn call(args: [*c]c.PJRT_Plugin_Attributes_Args) callconv(.c) ?*c.PJRT_Error {
+            var new_args = args.*;
+            new_args.struct_size = 0x18;
+            return api_inner.PJRT_Plugin_Attributes.?(&new_args);
+        }
+    }.call;
+
+    return &api_proxy;
 }
