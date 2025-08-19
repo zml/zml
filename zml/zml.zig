cd8a65334f4eb817540a3565acaa266f9074d42e
Steeve Morin <steeve@zml.ai>
2025-08-20 00:15:55 +0200

    zml: remove nemo, sentencepiece loaders, remove zig-yaml (#310)
    Progress towards #298

--- zml/zml.zig
+++ zml/zml.zig
@@ -3,37 +3,20 @@
 //! compiling it for various accelerators and targets, and executing it.
 //!
 
+// Namespaces
+const std = @import("std");
+
+pub const tokenizer = @import("zml/tokenizer");
+
+pub const aio = @import("aio.zig");
 pub const Buffer = @import("buffer.zig").Buffer;
 pub const Bufferized = @import("tensor.zig").Bufferized;
 pub const CompilationOptions = @import("platform.zig").CompilationOptions;
+pub const context = @import("context.zig");
 pub const Context = @import("context.zig").Context;
 pub const Data = @import("dtype.zig").Data;
 pub const DataType = @import("dtype.zig").DataType;
-pub const HostBuffer = @import("hostbuffer.zig").HostBuffer;
-pub const Platform = @import("platform.zig").Platform;
-pub const Shape = @import("shape.zig").Shape;
-pub const ShapeOf = @import("tensor.zig").ShapeOf;
-pub const Target = @import("platform.zig").Target;
-pub const Tensor = @import("tensor.zig").Tensor;
-
-// Namespaces
-pub const context = @import("context.zig");
 pub const exe = @import("exe.zig");
-pub const floats = @import("floats.zig");
-pub const helpers = @import("helpers.zig");
-pub const nn = @import("nn.zig");
-pub const module = @import("module.zig");
-pub const meta = @import("meta.zig");
-pub const platform = @import("platform.zig");
-pub const mlir = @import("mlirx.zig");
-pub const pjrt = @import("pjrtx.zig");
-pub const testing = @import("testing.zig");
-pub const torch = @import("torch.zig");
-
-// pub const tokenizer = @import("tokenizer.zig");
-pub const tokenizer = @import("zml/tokenizer");
-
-pub const call = ops.call;
 pub const compile = exe.compile;
 pub const compileWithPrefix = exe.compileWithPrefix;
 pub const compileFn = exe.compileFn;
@@ -41,18 +24,31 @@ pub const compileModel = exe.compileModel;
 pub const FnExe = exe.FnExe;
 pub const ModuleExe = exe.ModuleExe;
 pub const ModuleSignature = exe.ModuleSignature;
-
+pub const floats = @import("floats.zig");
+pub const helpers = @import("helpers.zig");
+pub const HostBuffer = @import("hostbuffer.zig").HostBuffer;
+pub const meta = @import("meta.zig");
+pub const mlir = @import("mlirx.zig");
+pub const module = @import("module.zig");
+pub const nn = @import("nn.zig");
 pub const ops = @import("ops.zig");
+pub const call = ops.call;
+pub const pjrt = @import("pjrtx.zig");
+pub const platform = @import("platform.zig");
+pub const Platform = @import("platform.zig").Platform;
+pub const Shape = @import("shape.zig").Shape;
+pub const ShapeOf = @import("tensor.zig").ShapeOf;
+pub const Target = @import("platform.zig").Target;
+pub const Tensor = @import("tensor.zig").Tensor;
+pub const testing = @import("testing.zig");
+pub const torch = @import("torch.zig");
+
+// pub const tokenizer = @import("tokenizer.zig");
 pub const tools = struct {
     pub const Tracer = @import("tools/tracer.zig").Tracer;
 };
 
-pub const aio = @import("aio.zig");
-pub const sentencepiece = @import("aio/sentencepiece.zig");
-
 pub const log = std.log.scoped(.zml);
-
-const std = @import("std");
 
 test {
     // NOTE : testing entrypoint.
