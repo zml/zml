5eac1f7ae435dc7b91e6a90d77cf2fb71ce7932f
Corentin Godeau <corentin.godeau@zml.ai>
2025-08-29 17:04:25 +0200

    wip: using Tensor pointer address

--- mlir/main.zig
+++ mlir/main.zig
@@ -1,109 +1,169 @@
 const std = @import("std");
+const stdx = @import("stdx");
+const zml = @import("zml");
+
+const TensorOriginKind = enum {
+    argument,
+    value,
+};
+
+const TensorOrigin = union(TensorOriginKind) {
+    argument: void,
+    value: usize,
+};
+
+var global_compilation_context: ?*CompilationContext = null;
+
+pub const Tensor = struct {
+    shape_: ?zml.Shape = null,
+    tensor_origin: TensorOrigin = .{ .argument = {} },
+
+    pub fn init(shape_: zml.Shape) Tensor {
+        return .{ .shape_ = shape_ };
+    }
+
+    pub fn matmul(self: *const Tensor, other: *const Tensor) Tensor {
+        const self_value = self.getValue();
+        const other_value = other.getValue();
+
+        std.debug.print("self: {} - other: {}\n", .{ self_value, other_value });
+        return .{};
+    }
+
+    pub fn add(self: *const Tensor, other: *const Tensor) Tensor {
+        _ = self; // autofix
+        _ = other; // autofix
+        return .{ .tensor_origin = .{ .value = 1 } };
+    }
+
+    pub fn relu(self: *const Tensor) Tensor {
+        _ = self; // autofix
+        return .{ .tensor_origin = .{ .value = 1 } };
+    }
+
+    pub fn flattenAll(self: *const Tensor) Tensor {
+        _ = self; // autofix
+        return .{ .tensor_origin = .{ .value = 1 } };
+    }
+
+    pub fn convert(self: *const Tensor, dtype: zml.DataType) Tensor {
+        _ = self; // autofix
+        _ = dtype; // autofix
+        return .{ .tensor_origin = .{ .value = 1 } };
+    }
+
+    pub fn argMax(self: *const Tensor, dim: i64) struct { indices: Tensor } {
+        _ = self; // autofix
+        _ = dim; // autofix
+        return .{ .indices = .{ .tensor_origin = .{ .value = 1 } } };
+    }
+
+    pub fn getValue(self: *const Tensor) usize {
+        return switch (self.tensor_origin) {
+            .argument => b: {
+                const entry = global_compilation_context.?.mapping.get(self).?;
+                break :b entry * 1000;
+            },
+            .value => |v| v,
+        };
+    }
+};
+
+/// Model definition
+const Mnist = struct {
+    fc1: Layer = .{},
+    fc2: Layer = .{},
+
+    const Layer = struct {
+        weight: Tensor = .{},
+        bias: Tensor = .{},
+
+        pub fn forward(self: *const Layer, input: *const Tensor) Tensor {
+            return self.weight.matmul(input).add(&self.bias).relu();
+        }
+    };
+
+    pub fn init() Mnist {
+        return .{};
+    }
+
+    /// just two linear layers + relu activation
+    pub fn forward(self: *const Mnist, input: *const Tensor) Tensor {
+        // std.log.info("Compiling for target: {s}", .{@tagName(input.getContext().target())});
+        var x = input.flattenAll().convert(.f32);
+        const layers: []const *const Layer = &.{ &self.fc1, &self.fc2 };
+        for (layers) |layer| {
+            x = layer.forward(&x);
+        }
+        return x.argMax(0).indices.convert(.u8);
+    }
+};
 
 const dialects = @import("mlir/dialects");
 const mlir = @import("mlir");
 
 pub fn main() !void {
     const allocator = std.heap.c_allocator;
-    const registry = try mlir.DialectRegistry.init();
-    defer registry.deinit();
 
-    // const handle = mlir.DialectHandle.fromString("func");
+    var mnist: Mnist = .init();
+    _ = &mnist;
 
-    inline for (.{ "memref", "func", "stablehlo" }) |d| {
-        registry.registerDialect(d);
+    // TODO(Corentin): Hydrate the model using the config/bufferstore
+
+    const input: Tensor = .init(zml.Shape.init(.{ 28, 28 }, .u8));
+
+    compileModel(allocator, Mnist.forward, &mnist, .{&input});
+}
+
+const CompilationContext = struct {
+    allocator: std.mem.Allocator,
+
+    mapping: std.AutoArrayHashMapUnmanaged(*const Tensor, usize),
+
+    pub fn init(allocator: std.mem.Allocator) CompilationContext {
+        return .{
+            .allocator = allocator,
+            .mapping = .{},
+        };
     }
 
-    // mlir.registerPasses("AllStablehlo");
+    pub fn deinit(self: *CompilationContext) void {
+        self.mapping.deinit(self.allocator);
+    }
 
-    const ctx = try mlir.Context.init(.{
-        .registry = registry,
-        .threading = true,
-    });
-    defer ctx.deinit();
+    pub fn appendToMapping(self: *CompilationContext, tensor: *const Tensor, unique_id: usize) void {
+        const gop = self.mapping.getOrPut(self.allocator, tensor) catch unreachable;
+        std.debug.assert(gop.found_existing == false);
 
-    std.debug.print(">>> {f}\n", .{ctx});
-    ctx.loadAllAvailableDialects();
-    std.debug.print(">>> {f}\n", .{ctx});
+        gop.value_ptr.* = unique_id;
+    }
+};
 
-    const unk = mlir.Location.unknown(ctx);
-    _ = unk; // autofix
+const Context = struct {
+    compilation_context: *CompilationContext,
+    current_id: usize = 0,
+};
 
-    var serialized: std.Io.Writer.Allocating = .init(allocator);
-    defer serialized.deinit();
+fn compileModel(allocator: std.mem.Allocator, comptime func: anytype, model: stdx.meta.Head(stdx.meta.FnArgs(func)), args: stdx.meta.Tail(stdx.meta.FnArgs(func))) void {
+    var compilation_context: CompilationContext = .init(allocator);
+    defer compilation_context.deinit();
 
-    // const stablehlo_version = blk: {
-    //     if (dialects.stablehlo.currentVersion()) |requested_version| {
-    //         break :blk dialects.stablehlo.stablehloGetSmallerVersion(requested_version, dialects.stablehlo.getCurrentVersion());
-    //     }
-    //     break :blk dialects.stablehlo.minimumVersion();
-    // };
+    global_compilation_context = &compilation_context;
+    defer global_compilation_context = null;
 
-    // const attr = mlir.integerAttribute(ctx, @as(i32, 5));
-    // _ = attr; // autofix
+    var context: Context = .{ .compilation_context = &compilation_context };
+    zml.meta.visit(struct {
+        fn cb(inner_context: *Context, tensor: *const Tensor) void {
+            inner_context.compilation_context.appendToMapping(tensor, inner_context.current_id);
+            inner_context.current_id += 1;
+        }
+    }.cb, &context, model);
+    zml.meta.visit(struct {
+        fn cb(inner_context: *Context, tensor: *const Tensor) void {
+            inner_context.compilation_context.appendToMapping(tensor, inner_context.current_id);
+            inner_context.current_id += 1;
+        }
+    }.cb, &context, &args);
 
-    // const one: i32 = 1;
-    // const two: i32 = 2;
-
-    // const dict = mlir.DictionaryAttribute.init(ctx, &.{
-    //     .named(ctx, "pouet", mlir.integerAttribute(ctx, one)),
-    //     .named(ctx, "pouet2", mlir.integerAttribute(ctx, two)),
-    // });
-    // _ = dict; // autofix
-
-    const module = mlir.Module.init(.unknown(ctx));
-    defer module.deinit();
-
-    const tensor_i32 = mlir.rankedTensorType(&.{ 4096, 4096 }, mlir.integerType(ctx, .i32));
-
-    _ = dialects.func.func(ctx, .{
-        .name = "pouet",
-        .visibility = .private,
-        .block = blk: {
-            const body = mlir.Block.init(&.{ tensor_i32, tensor_i32 }, &.{ .unknown(ctx), .unknown(ctx) });
-            const add_op = dialects.stablehlo.add(ctx, body.argument(0), body.argument(1), .unknown(ctx)).appendTo(body);
-            const mul_op = dialects.stablehlo.multiply(ctx, add_op.result(0), body.argument(1), .unknown(ctx)).appendTo(body);
-            _ = dialects.func.return_(ctx, mul_op.result(0), .unknown(ctx)).appendTo(body);
-            break :blk body;
-        },
-        .location = .unknown(ctx),
-    }).appendTo(module.body());
-
-    std.debug.print(">>>>>>\n{f}\n", .{module.operation().fmt(.{ .print_generic_op_form = false })});
-    std.debug.print(">>>>>>\n{f}\n", .{module.operation().fmt(.{ .print_generic_op_form = true })});
-
-    // dialects.stablehlo.serializePortableArtifact2(module, dialects.stablehlo.minimumVersion(), &serialized.writer) catch |err| {
-    //     std.debug.print("failed to serialize to portable artifact: {}\n", .{err});
-    //     return err;
-    // };
-
-    // const body = mod.body();
-    // _ = body.addArgument(mlir.integerType(ctx, 64, .signless), .unknown(ctx));
-    // _ = body.addArgument(mlir.integerType(ctx, 64, .signless), .unknown(ctx));
-    // _ = body.addArgument(mlir.integerType(ctx, 64, .signless), .unknown(ctx));
-
-    // body.appendOwnedOperation(op);
-
-    // op.deinit();
-    // body.detach();
-    // body.deinit();
-
-    // std.debug.print(">>>>>> {any}\n", .{op.block().?.parentOperation()});
-
-    // std.debug.print(">>>>>> {any}\n", .{op.block().?.parentOperation()});
-
-    // std.debug.print(">>> {f}\n", .{body});
-    // const attr = mlir.integerAttribute(ctx, @as(i32, 5));
-    // const attr2: *const mlir.Attribute = @ptrCast(attr);
-
-    // std.debug.print("coucou 2 {any} {f}\n", .{
-    //     ctx.isRegisteredOperation("func.func"),
-    //     attr2,
-    // });
-
-    // var buffer: [256]u8 = undefined;
-    // const vv = "coucou\n";
-    // var writer = std.fs.File.stdout().writer(&buffer);
-    // try writer.interface.writeAll(vv);
-    // try writer.interface.flush();
+    _ = @call(.auto, func, .{model} ++ args);
 }
