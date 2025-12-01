972c0a4aaa70ef91dafee074467451ce8f2f2d42
Corentin Godeau <corentin.godeau@zml.ai>
2025-12-01 15:51:38 +0100

    mlir and dialects modules rework
    * Complete rework of mlir that uses pointers instead of values for basic
      types.
    * Removed unused dialects.

--- mlir/dialects/dialects.zig
+++ mlir/dialects/dialects.zig
@@ -1,11 +1,7 @@
 const std = @import("std");
 
-pub const arith = @import("arith.zig");
-pub const func = @import("func.zig");
-pub const math = @import("math.zig");
-pub const scf = @import("scf.zig");
-pub const tensor = @import("tensor.zig");
 pub const stablehlo = @import("mlir/dialects/stablehlo");
+pub const func = @import("func.zig");
 
 test {
     std.testing.refAllDecls(@This());
