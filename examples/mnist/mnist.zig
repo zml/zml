6df392bbe2651844946855425dac0f727eedf7af
Corentin Godeau <corentin.godeau@zml.ai>
2025-12-15 16:10:50 +0100

    First port of Mnist to new API
    Everything is done by hand so that's not the final version, but at least
    it works!

--- examples/mnist/mnist.zig
+++ examples/mnist/mnist.zig
@@ -1,13 +1,12 @@
 const std = @import("std");
+const stdx = @import("stdx");
 
-const async = @import("async");
 const zml = @import("zml");
 
 const log = std.log.scoped(.mnist);
 
 pub const std_options: std.Options = .{
     .log_level = .info,
-    .logFn = async.logFn(std.log.defaultLog),
 };
 
 /// Model definition
@@ -20,14 +19,108 @@ const Mnist = struct {
         bias: zml.Tensor,
 
         pub fn forward(self: Layer, input: zml.Tensor) zml.Tensor {
-            return self.weight.matmul(input).add(self.bias).relu();
+            return self.weight.dot(input, .d).add(self.bias).relu().withTags(.{.d});
         }
     };
+
+    pub fn init(registry: *const zml.safetensors.TensorRegistry) Mnist {
+        const fc1_weight = b: {
+            const desc = registry.tensors.get("fc1.weight").?;
+            break :b zml.Tensor.fromShape(desc.shape).withTags(.{ .d_out, .d });
+        };
+        const fc1_bias: zml.Tensor = b: {
+            const desc = registry.tensors.get("fc1.bias").?;
+            break :b zml.Tensor.fromShape(desc.shape).withTags(.{.d_out});
+        };
+        const fc2_weight = b: {
+            const desc = registry.tensors.get("fc2.weight").?;
+            break :b zml.Tensor.fromShape(desc.shape).withTags(.{ .d_out, .d });
+        };
+        const fc2_bias: zml.Tensor = b: {
+            const desc = registry.tensors.get("fc2.bias").?;
+            break :b zml.Tensor.fromShape(desc.shape).withTags(.{.d_out});
+        };
+        return .{
+            .fc1 = .{ .weight = fc1_weight, .bias = fc1_bias },
+            .fc2 = .{ .weight = fc2_weight, .bias = fc2_bias },
+        };
+    }
+
+    pub fn deinit(self: *const Mnist) void {
+        _ = self; // autofix
+    }
+
+    pub fn loadBuffers(self: Mnist, allocator: std.mem.Allocator, io: std.Io, vfs: *zml.io.VFS, registry: *zml.safetensors.TensorRegistry, platform: zml.Platform) !zml.Bufferized(Mnist) {
+        _ = self; // autofix
+        const read_buffer = try allocator.alloc(u8, 4096);
+        defer allocator.free(read_buffer);
+
+        const fc1_weight: zml.Buffer = b: {
+            const desc = registry.tensors.get("fc1.weight").?;
+            const slice = try zml.Slice.alloc(allocator, desc.shape);
+            defer slice.free(allocator);
+
+            var writer = std.Io.Writer.fixed(slice.data);
+            var reader = try registry.reader(io, vfs, "fc1.weight", read_buffer);
+
+            _ = try reader.interface.streamRemaining(&writer);
+            break :b try .fromSlice(io, platform, slice);
+        };
+        errdefer fc1_weight.deinit();
+        const fc1_bias: zml.Buffer = b: {
+            const desc = registry.tensors.get("fc1.bias").?;
+            const slice = try zml.Slice.alloc(allocator, desc.shape);
+            defer slice.free(allocator);
+
+            var writer = std.Io.Writer.fixed(slice.data);
+            var reader = try registry.reader(io, vfs, "fc1.bias", read_buffer);
+
+            _ = try reader.interface.streamRemaining(&writer);
+            break :b try .fromSlice(io, platform, slice);
+        };
+        errdefer fc1_bias.deinit();
+        const fc2_weight: zml.Buffer = b: {
+            const desc = registry.tensors.get("fc2.weight").?;
+            const slice = try zml.Slice.alloc(allocator, desc.shape);
+            defer slice.free(allocator);
+
+            var writer = std.Io.Writer.fixed(slice.data);
+            var reader = try registry.reader(io, vfs, "fc2.weight", read_buffer);
+
+            _ = try reader.interface.streamRemaining(&writer);
+            break :b try .fromSlice(io, platform, slice);
+        };
+        errdefer fc2_weight.deinit();
+        const fc2_bias: zml.Buffer = b: {
+            const desc = registry.tensors.get("fc2.bias").?;
+            const slice = try zml.Slice.alloc(allocator, desc.shape);
+            defer slice.free(allocator);
+
+            var writer = std.Io.Writer.fixed(slice.data);
+            var reader = try registry.reader(io, vfs, "fc2.bias", read_buffer);
+
+            _ = try reader.interface.streamRemaining(&writer);
+            break :b try .fromSlice(io, platform, slice);
+        };
+        errdefer fc2_bias.deinit();
+
+        return .{
+            .fc1 = .{ .weight = fc1_weight, .bias = fc1_bias },
+            .fc2 = .{ .weight = fc2_weight, .bias = fc2_bias },
+        };
+    }
+
+    pub fn unloadBuffers(self: *zml.Bufferized(Mnist)) void {
+        self.fc1.weight.deinit();
+        self.fc1.bias.deinit();
+        self.fc2.weight.deinit();
+        self.fc2.bias.deinit();
+    }
 
     /// just two linear layers + relu activation
     pub fn forward(self: Mnist, input: zml.Tensor) zml.Tensor {
         // std.log.info("Compiling for target: {s}", .{@tagName(input.getContext().target())});
-        var x = input.flatten().convert(.f32);
+        var x = input.flatten().convert(.f32).withTags(.{.d});
         const layers: []const Layer = &.{ self.fc1, self.fc2 };
         for (layers) |layer| {
             x = layer.forward(x);
@@ -37,75 +130,89 @@ const Mnist = struct {
 };
 
 pub fn main() !void {
-    try async.AsyncThread.main(std.heap.c_allocator, asyncMain);
-}
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
-pub fn asyncMain() !void {
-    const allocator = std.heap.c_allocator;
+    const allocator = gpa.allocator();
 
-    // // Create ZML context
-    var context = try zml.Context.init();
-    defer context.deinit();
+    var threaded: std.Io.Threaded = .init(allocator);
+    defer threaded.deinit();
 
-    // log.info("\n===========================\n==   ZML MNIST Example   ==\n===========================\n\n", .{});
+    var vfs_file: zml.io.VFS.File = .init(threaded.io());
 
-    // // Auto-select platform
-    const platform = context.autoPlatform(.{});
-    context.printAvailablePlatforms(platform);
+    var vfs: zml.io.VFS = .init(allocator, threaded.io());
+    defer vfs.deinit();
+
+    try vfs.register("file", vfs_file.io());
+
+    const io = vfs.io();
+
+    zml.init();
+    defer zml.deinit();
+
+    // Auto-select platform
+    const platform: zml.Platform = try .auto(threaded.io(), .{});
 
     // Parse program args
     const process_args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, process_args);
-    const pt_model = process_args[1];
+    const model_path = process_args[1];
     const t10kfilename = process_args[2];
 
-    // Memory arena dedicated to model shapes and weights
-    var arena_state = std.heap.ArenaAllocator.init(allocator);
-    defer arena_state.deinit();
-    const arena = arena_state.allocator();
-
     // Read model shapes.
-    // Note this works because Mnist struct uses the same layer names as the pytorch model
-    var buffer_store = try zml.aio.detectFormatAndOpen(allocator, pt_model);
-    defer buffer_store.deinit();
+    var registry = try zml.safetensors.parseFromPath(allocator, io, &vfs, model_path);
+    defer registry.deinit();
 
-    const mnist_model = try zml.aio.populateModel(Mnist, allocator, buffer_store);
-    log.info("Reading model shapes from PyTorch file {s}...", .{pt_model});
+    // Init model
+    const mnist_model = Mnist.init(&registry);
+    defer mnist_model.deinit();
 
-    // Start compiling
+    // Compile model
     log.info("Compiling model to MLIR....", .{});
-    var start_time = try std.time.Timer.start();
-    var compilation = try async.async(zml.compile, .{ allocator, Mnist.forward, .{}, .{zml.Shape.init(.{ 28, 28 }, .u8)}, buffer_store, platform });
+    var timer = try stdx.time.Timer.start();
+    const input: zml.Tensor = .init(.{ 28, 28 }, .u8);
+    var exe = try platform.compileModel(allocator, io, Mnist.forward, mnist_model, .{input});
+    defer exe.deinit();
 
-    // While compiling, start loading weights on the platform
-    var model_weights = try zml.aio.loadModelBuffers(Mnist, mnist_model, buffer_store, arena, platform);
-    defer zml.aio.unloadBuffers(&model_weights);
+    log.info("✅ Compiled model in {f}", .{timer.read()});
 
-    // Wait for end of compilation and end of weights loading.
-    const compiled_mnist = try compilation.await();
-    log.info("✅ Compiled model in {d}ms", .{start_time.read() / std.time.ns_per_ms});
+    var args = try exe.args(allocator);
+    defer args.deinit(allocator);
 
-    const mnist = compiled_mnist.prepare(model_weights);
-    defer mnist.deinit();
-    log.info("✅ Weights transferred in {d}ms", .{start_time.read() / std.time.ns_per_ms});
+    var results = try exe.results(allocator);
+    defer results.deinit(allocator);
+
+    // Load buffers
+    var mnist_buffers = try mnist_model.loadBuffers(allocator, io, &vfs, &registry, platform);
+    defer Mnist.unloadBuffers(&mnist_buffers);
+    log.info("✅ Weights transferred in {f}", .{timer.read()});
 
     log.info("Starting inference...", .{});
 
     // Load a random digit image from the dataset.
-    const dataset = try async.File.open(t10kfilename, .{ .mode = .read_only });
-    defer dataset.close() catch unreachable;
-    var rng = std.Random.Xoshiro256.init(@intCast(std.time.timestamp()));
+    const dataset = try vfs.openAbsoluteFile(io, t10kfilename, .{ .mode = .read_only });
+    defer dataset.close(io);
+
+    const now = std.Io.Clock.now(.awake, io) catch unreachable;
+    var rng = std.Random.Xoshiro256.init(@intCast(now.toMilliseconds()));
 
     // inference - can be looped
     {
         const idx = rng.random().intRangeAtMost(u64, 0, 10000 - 1);
         var sample: [28 * 28]u8 align(16) = undefined;
-        _ = try dataset.pread(&sample, 16 + (idx * 28 * 28));
-        var input = try zml.Buffer.from(platform, zml.HostBuffer.fromBytes(zml.Shape.init(.{ 28, 28 }, .u8), &sample), .{});
-        defer input.deinit();
+        var reader = dataset.reader(io, &.{});
+        try reader.seekTo(16 + (idx * 28 * 28));
+        _ = try reader.interface.readSliceShort(&sample);
+
+        var input_buffer: zml.Buffer = try .fromSlice(io, platform, zml.Slice.init(input.shape(), &sample));
+        defer input_buffer.deinit();
+
+        args.set(.{ mnist_buffers, input_buffer });
 
         printDigit(sample);
-        var result: zml.Buffer = mnist.call(.{input});
+        exe.call(args, &results, io);
+
+        var result: zml.Buffer = results.get(zml.Buffer);
         defer result.deinit();
 
         log.info(
@@ -114,7 +221,7 @@ pub fn asyncMain() !void {
             \\{s}
             \\                       +-------------+
             \\
-        , .{digits[try result.getValue(u8)]});
+        , .{digits[try result.getValue(u8, io)]});
     }
 }
 
