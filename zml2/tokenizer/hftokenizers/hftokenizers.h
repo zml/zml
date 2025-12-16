850c8f31529437e7d5ee50064ae6bc5acf693077
Corentin Godeau <corentin.godeau@zml.ai>
2025-12-16 17:58:02 +0100

    Re-introduced zml/tokenizer

---
+++ zml2/tokenizer/hftokenizers/hftokenizers.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "ffi/zig_slice.h"
+
+typedef struct hftokenizers hftokenizers;
+
+hftokenizers *hftokenizers_new(zig_slice);
+hftokenizers *hftokenizers_new_from_bytes(zig_slice);
+void hftokenizers_drop(hftokenizers *tokenizer);
+zig_slice hftokenizers_encode(hftokenizers *tokenizer, zig_slice text);
+void hftokenizers_tokens_drop(zig_slice tokens);
+zig_slice hftokenizers_decode(hftokenizers *tokenizer, zig_slice tokens);
+void hftokenizers_str_drop(zig_slice text);
+uint32_t hftokenizers_token_to_id(hftokenizers *tokenizer, zig_slice token);
