b1f12e194a7563cc7aadc29edd775376f72fcdd7
Corentin Godeau <corentin@zml.ai>
2026-01-27 09:51:54 +0000

    Added paged attention FA3

--- zml/attention/paged_attention.zig
+++ zml/attention/paged_attention.zig
@@ -7,10 +7,12 @@ const PagedAttention = @This();
 
 pub const Backend = enum {
     cuda_fa2,
+    cuda_fa3,
 };
 
 pub const Options = union(Backend) {
     cuda_fa2: flashattn.paged_fa2.Options,
+    cuda_fa3: flashattn.paged_fa3.Options,
 
     pub fn isPrefill(self: Options) bool {
         return switch (self) {
@@ -21,16 +23,19 @@ pub const Options = union(Backend) {
 
 pub const Parameters = union(Backend) {
     cuda_fa2: flashattn.paged_fa2.Parameters,
+    cuda_fa3: flashattn.paged_fa3.Parameters,
 
     pub fn init(options_: Options) Parameters {
         return switch (options_) {
             .cuda_fa2 => |cuda_fa2_options| .{ .cuda_fa2 = flashattn.paged_fa2.Parameters.init(cuda_fa2_options) },
+            .cuda_fa3 => |cuda_fa3_options| .{ .cuda_fa3 = flashattn.paged_fa3.Parameters.init(cuda_fa3_options) },
         };
     }
 
     pub fn options(self: Parameters) Options {
         return switch (self) {
             .cuda_fa2 => |v| .{ .cuda_fa2 = v.options() },
+            .cuda_fa3 => |v| .{ .cuda_fa3 = v.options() },
         };
     }
 };
@@ -38,10 +43,12 @@ pub const Parameters = union(Backend) {
 /// Internal state that can be used inside the model code. It's derived from Parameters and Option.
 pub const Context = union(Backend) {
     cuda_fa2: flashattn.paged_fa2.Context,
+    cuda_fa3: flashattn.paged_fa3.Context,
 
     pub fn init(parameters: Parameters, num_heads: i64, num_kv_heads: i64, head_dim: i64, page_size: i64) Context {
         return switch (parameters) {
             .cuda_fa2 => |cuda_fa2_parameters| .{ .cuda_fa2 = flashattn.paged_fa2.Context.init(cuda_fa2_parameters, num_heads, num_kv_heads, head_dim, page_size) },
+            .cuda_fa3 => |cuda_fa3_parameters| .{ .cuda_fa3 = flashattn.paged_fa3.Context.init(cuda_fa3_parameters, num_heads, num_kv_heads, head_dim, page_size) },
         };
     }
 };
@@ -51,5 +58,6 @@ pub fn pagedAttention(parameters: Parameters, context: Context, q: zml.Tensor, k
     _ = v; // autofix
     return switch (parameters) {
         .cuda_fa2 => |cuda_fa2_parameters| flashattn.paged_fa2.pagedAttention(cuda_fa2_parameters, context.cuda_fa2, q, k_cache, v_cache, layer_index),
+        .cuda_fa3 => |cuda_fa3_parameters| flashattn.paged_fa3.pagedAttention(cuda_fa3_parameters, context.cuda_fa3, q, k_cache, v_cache, layer_index),
     };
 }
