05e86f82e827641a7c03eca83c859ac5ebd4eae9
Corentin Godeau <corentin@zml.ai>
2026-02-09 10:56:59 +0000

    mlir: better fix for custom_call aliases

--- mlir/dialects/stablehlo/stablehlo.zig
+++ mlir/dialects/stablehlo/stablehlo.zig
@@ -762,8 +762,9 @@ pub fn custom_call(ctx: *mlir.Context, inputs: []const *const mlir.Value, result
     if (opts.output_operand_aliases) |output_operand_aliases| {
         var buffer: stdx.BoundedArray(*const mlir.Attribute, MAX_RESULTS) = .{};
         for (output_operand_aliases, 0..) |alias, output_index| {
+            const output_tuple_indices = if (result_types.len > 1) &[1]i64{@intCast(output_index)} else &.{};
             buffer.appendAssumeCapacity(
-                outputOperandAliasAttribute(ctx, .{ .operand_index = alias, .output_tuple_indices = &[1]i64{@intCast(output_index)} }),
+                outputOperandAliasAttribute(ctx, .{ .operand_index = alias, .output_tuple_indices = output_tuple_indices }),
             );
         }
         attrs.appendAssumeCapacity(
