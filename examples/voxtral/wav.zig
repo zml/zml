47720d48fb0a74d321aec6cbeaba706a38333ad4
Raphael NAHOUM <raphael@zml.ai>
2026-02-13 16:43:41 +0100

    Some cleanup

--- examples/voxtral/wav.zig
+++ examples/voxtral/wav.zig
@@ -37,7 +37,7 @@ pub const Chunk = struct {
     };
 };
 
-pub fn readPcmWav(allocator: std.mem.Allocator, reader: *std.Io.Reader, out: *std.ArrayList(u8)) !Chunk.Fmt {
+fn readPcmWav(allocator: std.mem.Allocator, reader: *std.Io.Reader, out: *std.ArrayList(u8)) !Chunk.Fmt {
     std.debug.assert((try reader.takeStruct(Chunk.Header, .little)).id == .riff);
     std.debug.assert((try reader.takeStruct(Chunk.Riff, .little)).format == .wave);
     var fmt: ?Chunk.Fmt = null;
@@ -62,11 +62,31 @@ pub fn readPcmWav(allocator: std.mem.Allocator, reader: *std.Io.Reader, out: *st
     return fmt.?;
 }
 
-// pub fn writePcmWav(writer: anytype, fmt: Chunk.Fmt, data: []const u8) !void {
-//     try writer.writeStruct(Chunk.Header{ .id = .riff, .size = @sizeOf(Chunk.Riff) });
-//     try writer.writeStruct(Chunk.Riff{ .format = .wave });
-//     try writer.writeStruct(Chunk.Header{ .id = .fmt, .size = @sizeOf(Chunk.Fmt) });
-//     try writer.writeStruct(fmt);
-//     try writer.writeStruct(Chunk.Header{ .id = .data, .size = @intCast(data.len) });
-//     try writer.writeAll(data);
-// }
+pub fn loadWav(allocator: std.mem.Allocator, reader: *std.Io.Reader) ![]const f32 {
+    var arena_state: std.heap.ArenaAllocator = .init(allocator);
+    defer arena_state.deinit();
+
+    const arena = arena_state.allocator();
+    var sample_list: std.ArrayList(u8) = .empty;
+
+    const wav_fmt = try readPcmWav(arena, reader, &sample_list);
+    const byte_per_sample = wav_fmt.bits_per_sample / 8;
+    const sample_count = sample_list.items.len / (byte_per_sample * wav_fmt.num_channels);
+
+    const samples = try allocator.alloc(f32, sample_count);
+    for (0..sample_count) |i| {
+	const offset = i * byte_per_sample * wav_fmt.num_channels;
+
+	const sample = switch(byte_per_sample) {
+	    1 => (@as(f32, @floatFromInt(std.mem.bytesToValue(u8, sample_list.items[offset .. offset + 1]))) - 128.0) / 128.0,
+	    2 => @as(f32, @floatFromInt(std.mem.bytesToValue(i16, sample_list.items[offset .. offset + 2]))) / 32768.0,
+	    3 => @as(f32, @floatFromInt(std.mem.bytesToValue(i24, sample_list.items[offset .. offset + 3]))) / 8388608.0,
+	    4 => @as(f32, @floatFromInt(std.mem.bytesToValue(i32, sample_list.items[offset .. offset + 4]))) / 2147483648.0,
+	    else => unreachable,
+	};
+
+	samples[i] = sample;
+    }
+
+    return samples;
+}
