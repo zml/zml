e258ecb1473109f279c0f5b58190b1fac1b00770
Benjamin Rabier <benjamin@zml.ai>
2026-03-03 17:40:23 +0100

    Add devenv files
    It defines my nix environment for this project, and it's a lot easier to
    manage by committing it.

---
+++ devenv.nix
@@ -0,0 +1,19 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  packages = with pkgs; [
+    bazelisk
+    python3
+    uv
+  ];
+  env.ZLS_CMD = "${config.devenv.root}/tools/zls.sh";
+  scripts.bazel.exec = ''bazelisk "$@"'';
+
+  enterShell = pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
+    # Use the "real" XCode rather than Nixpkg's one. This does assume you have it installed.
+    export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
+    export SDKROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk";
+  '';
+}
