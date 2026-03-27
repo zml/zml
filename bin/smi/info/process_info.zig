d88bc015c78b5678bbb0b9e48c4876b718af3a04
Raphael NAHOUM <raphael@zml.ai>
2026-03-27 21:43:15 +0100

    zml-smi: add arena to process enricher

--- bin/smi/info/process_info.zig
+++ bin/smi/info/process_info.zig
@@ -4,8 +4,8 @@ pub const ProcessInfo = struct {
     dev_util_percent: ?u16 = null,
     dev_mem_kib: ?u64 = null,
     uid: u32 = 0,
-    username: [32]u8 = .{0} ** 32,
-    comm: ?[4096]u8 = null,
+    username: []const u8 = "",
+    comm: []const u8 = "",
     cpu_percent: u16 = 0, // *10, e.g. 125 = 12.5%
     rss_kib: u64 = 0,
 };
