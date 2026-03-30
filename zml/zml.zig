f8da049ed1a9ef57546835ff428e4a6907c1893a
loupicaaa <louis@zml.ai>
2026-03-30 10:18:08 +0000

    Moe general backend implementation, triton moe backend for H100 added, resolving python 3.14 and triton dependencies in MODULE.bazel for ttir generation

--- zml/zml.zig
+++ zml/zml.zig
@@ -46,6 +46,8 @@ pub const tensor = @import("tensor.zig");
 pub const Tensor = tensor.Tensor;
 pub const testing = @import("testing.zig");
 pub const tracer = @import("tracer.zig");
+pub const triton_moe = @import("moe/triton.zig");
+pub const moe = @import("moe/moe.zig");
 
 test "zml" {
     std.testing.refAllDecls(@This());
