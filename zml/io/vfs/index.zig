8a4542f26b6c82346fd77b4560cca860cfd0cf6b
Steeve Morin <steeve@zml.ai>
2026-04-03 16:33:00 +0200

    zml/io/vfs: add GCS VFS
    Close to the S3 VFS, with a different authentication flow.
    It tries to mimick what the Go package does.

--- zml/io/vfs/index.zig
+++ zml/io/vfs/index.zig
@@ -3,6 +3,7 @@ const log = std.log.scoped(.@"zml/io/vfs/vfs");
 const std = @import("std");
 
 pub const File = @import("file.zig").File;
+pub const GCS = @import("gcs.zig").GCS;
 pub const HF = @import("hf.zig").HF;
 pub const HTTP = @import("http.zig").HTTP;
 pub const S3 = @import("s3.zig").S3;
