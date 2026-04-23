9461d17736627116c9d30e0f4320bc7a3b92ebf0
Kevin Barre <kevin@zml.ai>
2026-04-23 12:18:29 +0000

    gcs: enhance error handling for credential JSON initialization

--- zml/io/vfs/gcs.zig
+++ zml/io/vfs/gcs.zig
@@ -238,11 +238,14 @@ pub const GCS = struct {
             const creds = GCS.init(allocator, inner_io, http_client, .{ .credentials = .{ .json = &reader.interface } }) catch |err| switch (err) {
                 InitError.InvalidCredentialJson => {
                     var real_path_buf: [std.fs.max_path_bytes]u8 = undefined;
-                    const file_path_size: usize = try f.realPath(inner_io, &real_path_buf);
+                    const file_path_size: usize = f.realPath(inner_io, &real_path_buf) catch |real_path_err| {
+                        log.warn("Invalid GCS credential JSON (failed to resolve credential path: {!})", .{real_path_err});
+                        return .init(allocator, inner_io, http_client, .{});
+                    };
                     log.warn("Invalid GCS credential JSON at {s}", .{real_path_buf[0..file_path_size]});
                     return .init(allocator, inner_io, http_client, .{});
                 },
-                else => err,
+                else => return err,
             };
             return creds;
         }
