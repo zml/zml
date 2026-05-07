d2df27399a8bdb1b61590cf3d75410569e4bcf15
Reese Chong <reese@zml.ai>
2026-05-07 17:18:17 +0000

    examples/llm: attempt to fix hf strict modeling code

--- examples/llm/models/step3_5flash/main.py
+++ examples/llm/models/step3_5flash/main.py
@@ -1,17 +1,26 @@
 import torch
 import transformers
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 from safetensors.torch import save_file
 import zml_utils
 import os
 
 MODEL_PATH = "stepfun-ai/Step-3.5-Flash"
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+config = AutoConfig.from_pretrained(MODEL_PATH, trust_remote_code=True)
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, trust_remote_code=True)
+
+if isinstance(tokenizer.eos_token_id, list):
+    config.pad_token_id = tokenizer.eos_token_id[0]
+else:
+    config.pad_token_id = tokenizer.eos_token_id
+
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_PATH,
+    config=config,
     trust_remote_code=True,
-    torch_dtype="auto",
+    torch_dtype=torch.bfloat16,
     device_map="auto",
 )
 
@@ -24,7 +33,7 @@ inputs = tokenizer.apply_chat_template(
     return_tensors="pt",
 ).to(model.device)
 
-# 3. Generate
+# Generate
 generated_ids = model.generate(**inputs, max_new_tokens=128, do_sample=False)
 output_text = tokenizer.decode(generated_ids[0][inputs.input_ids.shape[1]:], skip_special_tokens=True)
 
