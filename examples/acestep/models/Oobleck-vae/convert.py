9991bd31d24dba7d474f9ba91b44a1112578cc43
Simon Boulmier <simon@zml.ai>
2026-05-13 17:10:42 +0200

    sync

--- examples/acestep/models/Oobleck-vae/convert.py
+++ examples/acestep/models/Oobleck-vae/convert.py
@@ -7,16 +7,39 @@ import torch
 from safetensors.torch import load_file, save_file
 
 def main() -> None:
-    input_path = "//Users//sboulmier//zml//examples//acestep//models//Oobleck-vae//diffusion_pytorch_model.safetensors"
-    output_path = "//Users//sboulmier//zml//examples//acestep//models//Oobleck-vae//diffusion_pytorch_model_f32.safetensors"
+    input_path_1 = "//Users//sboulmier//zml//examples//acestep//models//acestep-v15-xl-turbo//model-00001-of-00004.safetensors"
+    input_path_2 = "//Users//sboulmier//zml//examples//acestep//models//acestep-v15-xl-turbo//model-00002-of-00004.safetensors"
+    input_path_3 = "//Users//sboulmier//zml//examples//acestep//models//acestep-v15-xl-turbo//model-00003-of-00004.safetensors"
+    input_path_4 = "//Users//sboulmier//zml//examples//acestep//models//acestep-v15-xl-turbo//model-00004-of-00004.safetensors"
+    
+    output_path_1 = "//Users//sboulmier//zml//examples//acestep//models//acestep-v15-xl-turbo//model-00001-of-00004_bf16.safetensors"
+    output_path_2 = "//Users//sboulmier//zml//examples//acestep//models//acestep-v15-xl-turbo//model-00002-of-00004_bf16.safetensors"
+    output_path_3 = "//Users//sboulmier//zml//examples//acestep//models//acestep-v15-xl-turbo//model-00003-of-00004_bf16.safetensors"
+    output_path_4 = "//Users//sboulmier//zml//examples//acestep//models//acestep-v15-xl-turbo//model-00004-of-00004_bf16.safetensors"
 
-    tensors = load_file(input_path)
-    tensors_f32 = {name: tensor.to(dtype=torch.float32) for name, tensor in tensors.items()}
+    tensors_1 = load_file(input_path_1)
+    tensors_1_bf16 = { name: tensor.to(dtype=torch.bfloat16) for name, tensor in tensors_1.items()}
+    save_file(tensors_1_bf16, output_path_1)
+    print(f"Loaded {len(tensors_1)} tensors from: {input_path_1}")
+    print(f"Saved bf16 tensors to: {output_path_1}")
 
-    save_file(tensors_f32, output_path)
+    tensors_2 = load_file(input_path_2)
+    tensors_2_bf16 = { name: tensor.to(dtype=torch.bfloat16) for name, tensor in tensors_2.items()}
+    save_file(tensors_2_bf16, output_path_2)
+    print(f"Loaded {len(tensors_2)} tensors from: {input_path_2}")
+    print(f"Saved bf16 tensors to: {output_path_2}")
 
-    print(f"Loaded {len(tensors)} tensors from: {input_path}")
-    print(f"Saved float32 tensors to: {output_path}")
+    tensors_3 = load_file(input_path_3)
+    tensors_3_bf16 = { name: tensor.to(dtype=torch.bfloat16) for name, tensor in tensors_3.items()}
+    save_file(tensors_3_bf16, output_path_3)
+    print(f"Loaded {len(tensors_3)} tensors from: {input_path_3}")
+    print(f"Saved bf16 tensors to: {output_path_3}")
+
+    tensors_4 = load_file(input_path_4)
+    tensors_4_bf16 = { name: tensor.to(dtype=torch.bfloat16) for name, tensor in tensors_4.items()}
+    save_file(tensors_4_bf16, output_path_4)
+    print(f"Loaded {len(tensors_4)} tensors from: {input_path_4}")
+    print(f"Saved bf16 tensors to: {output_path_4}")
 
 if __name__ == "__main__":
     main()
