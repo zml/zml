c361a32248171396684ee03eb348255ada57f9c0
Simon Boulmier <simon@zml.ai>
2026-05-15 13:58:17 +0200

    faster wav export

--- examples/acestep/main.zig
+++ examples/acestep/main.zig
@@ -430,7 +430,7 @@ pub fn runFullPipeline(zml_handler: *Zml_handler) !void {
     
          zml_handler.tic(&zml_handler.timers.wav);
          
-         try exportDecodedAudioAsWav2(zml_handler, decoded_audio, i);
+         try exportDecodedAudioAsWav(zml_handler, decoded_audio, i);
     
          zml_handler.toc(&zml_handler.timers.wav);
     }
@@ -499,11 +499,14 @@ pub fn exportDecodedAudioAsWav(zml_handler: *Zml_handler, decoded_audio: inferen
     try writer.writeAll("data");
     try writer.writeInt(u32, data_chunk_size, .little);
 
+    const channel_1 = samples[0..num_frames];
+    const channel_2 = samples[num_frames..num_frames * 2];
+
     for (0..num_frames) |frame_idx| {
-        for (0..num_channels) |channel_idx| {
-            const sample = samples[channel_idx * num_frames + frame_idx];
-            try writer.writeInt(u32, @bitCast(sample), .little);
-        }
+        const sample_1 = channel_1[frame_idx];
+        const sample_2 = channel_2[frame_idx];
+        try writer.writeInt(u32, @bitCast(sample_1), .little);
+        try writer.writeInt(u32, @bitCast(sample_2), .little);
     }
 
     try writer.flush();
