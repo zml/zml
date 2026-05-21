f87a5219b53b9f3c41e0fd9f0008a87705327902
Simon Boulmier <simon@zml.ai>
2026-05-21 16:18:57 +0200

    sync

--- examples/acestep/acellm.zig
+++ examples/acestep/acellm.zig
@@ -727,10 +727,10 @@ const AttLayer = struct {
             // combine the two masks
             mask = range_mask.add(causal_mask);
         } else {
-            var attn_mask2 = zml.nn.causalAttnMask(.{ .q = k.dim(.k), .k = k.dim(.k) }, q.dtype(), null);
-            attn_mask2 = attn_mask2.gatherSlices(zml.Shape.init(.{ .q = q.dim(.q) }, attn_mask2.dtype()), token_index.reshape(.{ .coord = 1 }), .{});
-            mask = attn_mask2;
-            print(attn_mask2, '2');
+            //var attn_mask2 = zml.nn.causalAttnMask(.{ .q = k.dim(.k), .k = k.dim(.k) }, q.dtype(), null);
+            //attn_mask2 = attn_mask2.gatherSlices(zml.Shape.init(.{ .q = q.dim(.q) }, attn_mask2.dtype()), token_index.reshape(.{ .coord = 1 }), .{});
+            //mask = attn_mask2;
+            //print(attn_mask2, '2');
             
             // init causal mask : 1 x seq_len = [0...0]@[-inf ... -inf] at pos [0..token_index], [token_index..seq_len]
             //const zeros = zml.Tensor.zeroes(range_mask.shape());
