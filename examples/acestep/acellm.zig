1c6931ab211b884b62e5cc1a2451a757ce55c86a
Simon Boulmier <simon@zml.ai>
2026-05-21 16:17:32 +0200

    sync

--- examples/acestep/acellm.zig
+++ examples/acestep/acellm.zig
@@ -744,7 +744,7 @@ const AttLayer = struct {
             //mask = range_mask.add(causal_mask);
         }
         
-        const attn_heads_output = zml.nn.sdpa(q, k, v, .{ .attn_mask = mask });
+        const attn_heads_output = zml.nn.sdpa(q, k, v, .{ .attn_mask = null });
         const attn_output = attn_heads_output.merge(.{ .d = .{ .h, .hd } }).rename(.{ .q = .s });
         const delta = self.o_proj.forward(attn_output).rename(.{ .d_out = .d });
         
