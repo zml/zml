447a016a2092e7cb179396197fdc05ad0fea23e1
Reese Chong <reese@zml.ai>
2026-07-20 18:54:47 +0000

    select vulkan backend explicitly instead of cuda (might need to revert later)

--- zml/attention.zig
+++ zml/attention.zig
@@ -38,7 +38,7 @@ pub const Backend = enum {
             },
             .neuron => .nki,
             .metal => .metal_fa,
-            .cpu, .rocm, .tpu, .oneapi => .vanilla,
+            .cpu, .rocm, .tpu, .oneapi, .vulkan => .vanilla,
         };
     }
 
