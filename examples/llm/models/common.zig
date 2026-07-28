961307021fdf699c0d9dfea1dbc48c9243d2d1a7
Reese Chong <reese@zml.ai>
2026-07-28 12:07:19 +0000

    fixup! add vulkan to list of platforms across zml

--- examples/llm/models/common.zig
+++ examples/llm/models/common.zig
@@ -63,7 +63,7 @@ pub const Shardings = struct {
                     .experts = try platform.registerShardingWithStrategy("experts", .mesh(.{ .experts = .high_bandwidth }), strategy_experts),
                 };
             },
-            .cuda, .rocm, .oneapi, .neuron, .metal, .cpu => return .{
+            .cuda, .rocm, .oneapi, .neuron, .metal, .cpu, .vulkan => return .{
                 .model = try platform.registerSharding("model", .mesh(.{ .model = .high_bandwidth })),
                 .experts = try platform.registerSharding("experts", .mesh(.{ .experts = .high_bandwidth })),
             },
