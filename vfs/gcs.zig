4c92d88a014bf41d765b6628f3a48c2959fb92b3
Guillaume Wenzek <wzk@zml.ai>
2026-07-30 17:47:04 +0200

    zig 0.17: finish std API migrations

--- vfs/gcs.zig
+++ vfs/gcs.zig
@@ -652,7 +652,7 @@ pub const GCS = struct {
                 handle.pos += @intCast(total);
                 return .{ .file_read_streaming = total };
             },
-            .file_write_streaming, .device_io_control, .net_receive => {
+            .file_write_streaming, .device_io_control, .net_receive, .net_read => {
                 return self.base.inner.vtable.operate(self.base.inner.userdata, operation);
             },
         }
@@ -709,7 +709,7 @@ pub const GCS = struct {
 
     fn dirAccess(_: ?*anyopaque, _: std.Io.Dir, _: []const u8, _: std.Io.Dir.AccessOptions) std.Io.Dir.AccessError!void {}
 
-    fn dirOpenFile(userdata: ?*anyopaque, dir: std.Io.Dir, sub_path: []const u8, _: std.Io.File.OpenFlags) std.Io.File.OpenError!std.Io.File {
+    fn dirOpenFile(userdata: ?*anyopaque, dir: std.Io.Dir, sub_path: []const u8, _: std.Io.Dir.OpenFileOptions) std.Io.File.OpenError!std.Io.File {
         const self: *GCS = @alignCast(@fieldParentPtr("base", VFSBase.as(userdata)));
         const size = self.fetchSize(dir, sub_path) catch |err| switch (err) {
             error.FileNotFound => return std.Io.File.OpenError.FileNotFound,
