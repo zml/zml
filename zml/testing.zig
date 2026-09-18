d53999bdfb9b45719cfb3877ad4255cf02bf1b59
Corentin Godeau <corentin@zml.ai>
2026-09-18 16:16:56 +0000

    zml/testing: explicit allocator in env

--- zml/testing.zig
+++ zml/testing.zig
@@ -14,10 +14,16 @@ var _platform: ?*const Platform = null;
 pub fn env() *const Platform {
     if (!builtin.is_test) @compileError("Cannot use zml.testing.env outside of a test block");
     if (_platform == null) {
+        const memory_fraction = 0.90;
         const platform = Platform.auto(
             std.heap.c_allocator,
             std.testing.io,
-            .{},
+            .{
+                .cuda = .{ .allocator = .{ .vmm = .{ .memory_fraction = memory_fraction } } },
+                .rocm = .{ .allocator = .{ .vmm = .{ .memory_fraction = memory_fraction } } },
+                .metal = .{ .allocator = .{ .bfc = .{ .preallocate = true, .memory_fraction = memory_fraction } } },
+                .oneapi = .{ .allocator = .{ .bfc = .{ .preallocate = true, .memory_fraction = memory_fraction } } },
+            },
         ) catch @panic("Pjrt not available");
 
         // Register shardings required by flash attention.
