f87c27af48ff3a8d257c524d94fa4ea74b4d1730
Guillaume Wenzek <wzk@zml.ai>
2026-09-24 15:52:05 +0200

    stdx/fmt: don't truncate individual messages for `stdx.fmt.slice`

--- stdx/fmt.zig
+++ stdx/fmt.zig
@@ -15,7 +15,12 @@ fn FmtSlice(T: type) type {
         slice: []const T,
 
         pub fn format(f: @This(), writer: *std.Io.Writer) std.Io.Writer.Error!void {
-            return try formatSliceAny(f.slice, .{}, 1, writer);
+            _ = try writer.writeAll("{");
+            for (0.., f.slice) |i, data| {
+                if (i > 0) try writer.writeAll(", ");
+                try data.format(writer);
+            }
+            _ = try writer.writeAll("}");
         }
 
         pub fn formatNumber(f: @This(), writer: *std.Io.Writer, n: std.fmt.Number) std.Io.Writer.Error!void {
@@ -28,7 +33,7 @@ fn FmtSlice(T: type) type {
                 } else if (@hasDecl(T, "toF32")) {
                     try formatFloatSlice(f.slice, n, 1, writer);
                 } else {
-                    try formatSliceAny(f.slice, n, 1, writer);
+                    try formatSliceAnyNumber(f.slice, n, 1, writer);
                 },
                 else => @compileError("FmtSlice doesn't support type: " ++ @typeName(T)),
             };
@@ -98,8 +103,7 @@ pub fn formatStr(value: []const u8, spec: std.fmt.Number, writer: *std.Io.Writer
     try writer.alignBufferOptions(value, .{ .alignment = spec.alignment, .fill = spec.fill });
 }
 
-pub fn formatAny(value: anytype, spec: std.fmt.Number, writer: *std.Io.Writer) !void {
-    var buf: [48]u8 = undefined;
+pub fn formatAnyNumber(value: anytype, spec: std.fmt.Number, writer: *std.Io.Writer) !void {
     const T = @TypeOf(value);
     const fmt = switch (@typeInfo(T)) {
         .@"struct", .@"union", .@"enum", .@"opaque" => if (@hasDecl(T, "formatNumber"))
@@ -109,6 +113,7 @@ pub fn formatAny(value: anytype, spec: std.fmt.Number, writer: *std.Io.Writer) !
         else => "{f}",
     };
 
+    var buf: [48]u8 = undefined;
     const s = std.fmt.bufPrint(&buf, fmt, .{value}) catch blk: {
         buf[45..].* = "...".*;
         break :blk buf[0..];
@@ -147,8 +152,8 @@ pub fn formatSliceCustom(fmt_func: anytype, values: anytype, spec: std.fmt.Numbe
     _ = try writer.write("}");
 }
 
-pub fn formatSliceAny(values: anytype, spec: std.fmt.Number, stride: i64, writer: *std.Io.Writer) !void {
-    return try formatSliceCustom(formatAny, values, spec, stride, writer);
+pub fn formatSliceAnyNumber(values: anytype, spec: std.fmt.Number, stride: i64, writer: *std.Io.Writer) !void {
+    return try formatSliceCustom(formatAnyNumber, values, spec, stride, writer);
 }
 
 pub fn formatStrSlice(values: anytype, spec: std.fmt.Number, stride: i64, writer: *std.Io.Writer) !void {
